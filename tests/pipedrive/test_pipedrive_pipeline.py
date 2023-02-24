b4c5cc4a2a55624d8de5b4be66400e2feb90850a
nacho0605 <carlos.colombana@gmail.com>
2023-02-24 18:09:41 -0300

    Adds tests' basic version

--- tests/pipedrive/test_pipedrive_pipeline.py
+++ tests/pipedrive/test_pipedrive_pipeline.py
@@ -16,3 +16,32 @@ def test_all_resources(destination_name: str) -> None:
     print(load_info)
     assert_load_info(load_info)
     # TODO: validate schema and data: write a test helper for that
+
+
+@pytest.mark.parametrize('destination_name', ALL_DESTINATIONS)
+def test_custom_fields_munger(destination_name: str) -> None:
+
+    pipeline = dlt.pipeline(pipeline_name='pipedrive', destination=destination_name, dataset_name='pipedrive_data', full_refresh=True)
+
+    load_info = pipeline.run(pipedrive_source().with_resources('persons', 'personFields', 'products', 'productFields'))
+    print(load_info)
+    assert_load_info(load_info)
+
+    with pipeline.sql_client() as db_client:
+        query_string = "SELECT {select_field} FROM {table} WHERE {where_field}=%s"
+
+        with db_client.execute_query(query_string.format(select_field='key', table='person_fields', where_field='name'), 'MULTIPLE OPTIONS ') as cursor:
+            rows = list(cursor.fetchone())
+            assert rows[0] == 'multiple_options'
+
+        with db_client.execute_query(query_string.format(select_field='key', table='person_fields', where_field='name'), 'CUSTOM NAME I CHOSE') as cursor:
+            rows = list(cursor.fetchone())
+            assert rows[0] == 'custom_name_i_chose'
+
+        with db_client.execute_query(query_string.format(select_field='key', table='product_fields', where_field='name'), 'SPAM') as cursor:
+            rows = list(cursor.fetchone())
+            assert rows[0] == 'spam'
+
+        with db_client.execute_query(query_string.format(select_field='spam', table='products', where_field='spam'), 'SS') as cursor:
+            rows = list(cursor.fetchone())
+            assert rows[0] == 'SS'
