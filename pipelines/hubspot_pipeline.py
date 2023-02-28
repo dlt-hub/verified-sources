cae77cfeb898f042483855101c3256df5882653e
mebus <sherban.carlogea@gmail.com>
2023-02-28 12:28:18 +0100

    hubspot api source

---
+++ pipelines/hubspot_pipeline.py
@@ -0,0 +1,19 @@
+import dlt
+
+from pipelines.hubspot import hubspot
+
+
+def main():
+    p = dlt.pipeline(pipeline_name='hubspot',
+                     dataset_name='hubspot',
+                     destination='postgres',
+                     full_refresh=False)
+
+    info = p.run(
+        hubspot()
+    )
+    print(info)
+
+
+if __name__ == "__main__":
+    main()
