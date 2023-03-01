8ed444d09c286a87c3560acd8d8203ca75604b0c
Steinthor Palsson <steini90@gmail.com>
2023-02-28 19:43:30 -0500

    Test for sql_table resource simple case

--- tests/sql_database/test_sql_database_pipeline.py
+++ tests/sql_database/test_sql_database_pipeline.py
@@ -1,11 +1,13 @@
 import pytest
 import os
+from typing import List
 
 import dlt
 from dlt.common.configuration.specs import ConnectionStringCredentials
 from dlt.common.utils import uniq_id
+from dlt.extract.source import DltResource
 
-from pipelines.sql_database import sql_database
+from pipelines.sql_database import sql_database, sql_table
 
 from tests.utils import ALL_DESTINATIONS, assert_load_info
 from tests.sql_database.sql_source import SQLAlchemySourceDB
@@ -108,3 +110,28 @@ def test_load_sql_table_incremental(sql_source_db: SQLAlchemySourceDB, destinati
             with c.execute_query(f"SELECT count(*) FROM {table}") as cur:
                 row = cur.fetchone()
                 assert row[0] == info['row_count']
+
+
+
+@pytest.mark.parametrize("destination_name", ALL_DESTINATIONS)
+def test_load_sql_table_loads_data(sql_source_db: SQLAlchemySourceDB, destination_name: str) -> None:
+    @dlt.source
+    def sql_table_source() -> List[DltResource]:
+        return [
+            sql_table(credentials=sql_source_db.credentials, schema=sql_source_db.schema, table='chat_message')
+        ]
+
+    pipeline = dlt.pipeline(
+        pipeline_name='sql_database',
+        destination=destination_name,
+        dataset_name='test_sql_pipeline_' + uniq_id(),
+        full_refresh=False
+    )
+    load_info = pipeline.run(sql_table_source())
+    assert_load_info(load_info)
+
+    with pipeline.sql_client() as c:
+        info = sql_source_db.table_infos['chat_message']
+        with c.execute_query("SELECT count(*) FROM chat_message") as cur:
+            row = cur.fetchone()
+            assert row[0] == info['row_count']
