7c447ba4e910f175c8a257d5799cfa70d4201612
mebus <sherban.carlogea@gmail.com>
2023-03-13 10:32:19 +0100

    fixes for web analytics events

--- pipelines/hubspot/__init__.py
+++ pipelines/hubspot/__init__.py
@@ -1,17 +1,17 @@
+from datetime import datetime
 from typing import Sequence, Iterator
 
 import dlt
-
 from dlt.common.typing import TDataItems
 from dlt.extract.source import DltResource
 
 from .client import fetch_data
 from .endpoints import CRM_CONTACTS_ENDPOINT, CRM_COMPANIES_ENDPOINT, \
-    CRM_DEALS_ENDPOINT, CRM_TICKETS_ENDPOINT, CRM_PRODUCTS_ENDPOINT, CRM_ENGAGEMENTS_CALLS_ENDPOINT, \
-    CRM_ENGAGEMENTS_COMMUNICATIONS_ENDPOINT, CRM_ENGAGEMENTS_EMAILS_ENDPOINT, WEB_ANALYTICS_EVENTS_ENDPOINT, CRM_QUOTES_ENDPOINT
+    CRM_DEALS_ENDPOINT, CRM_TICKETS_ENDPOINT, CRM_PRODUCTS_ENDPOINT, WEB_ANALYTICS_EVENTS_ENDPOINT, \
+    CRM_QUOTES_ENDPOINT
 
 
-@dlt.source(name="hubspot")
+@dlt.source
 def hubspot(api_key: str = dlt.secrets.value) -> Sequence[DltResource]:
     """
     A DLT source that retrieves data from the HubSpot API using the specified API key.
@@ -37,32 +37,78 @@ def hubspot(api_key: str = dlt.secrets.value) -> Sequence[DltResource]:
 
     """
 
+    def _get_web_analytics_events(object_type: str, object_id: str):
+        _bookmark = dlt.state().setdefault(f"event_bookmark_{object_type}_{object_id}",
+                                           datetime(1970, 1, 1).strftime("%Y-%m-%dT%H:%M:%S.000Z"))
+
+        _event = None
+        for _event in fetch_data(
+            WEB_ANALYTICS_EVENTS_ENDPOINT.format(objectType=object_type, objectId=object_id,
+                                                 occurredAfter=_bookmark),
+            api_key=api_key
+        ):
+            yield _event
+
+        if _event and 'occurredAt' in _event:
+            dlt.state()[f"event_bookmark_{object_type}_{object_id}"] = _event['occurredAt']
+
+    @dlt.resource(write_disposition="append")
+    def companies_events() -> Iterator[TDataItems]:
+        for entity in fetch_data(CRM_COMPANIES_ENDPOINT, api_key=api_key):
+            yield _get_web_analytics_events("company", entity["hs_object_id"])
+
     @dlt.resource(write_disposition="replace")
     def companies() -> Iterator[TDataItems]:
         yield fetch_data(CRM_COMPANIES_ENDPOINT, api_key=api_key)
+
+    @dlt.resource(write_disposition="append")
+    def contacts_events() -> Iterator[TDataItems]:
+        for entity in fetch_data(CRM_CONTACTS_ENDPOINT, api_key=api_key):
+            yield _get_web_analytics_events("contact", entity["hs_object_id"])
 
     @dlt.resource(write_disposition="replace")
     def contacts() -> Iterator[TDataItems]:
         yield fetch_data(CRM_CONTACTS_ENDPOINT, api_key=api_key)
 
+    @dlt.resource(write_disposition="append")
+    def deals_events():
+        for entity in fetch_data(CRM_DEALS_ENDPOINT, api_key=api_key):
+            yield _get_web_analytics_events("deal", entity["hs_object_id"])
+
     @dlt.resource(write_disposition="replace")
     def deals() -> Iterator[TDataItems]:
         yield fetch_data(CRM_DEALS_ENDPOINT, api_key=api_key)
+
+    @dlt.resource(write_disposition="append")
+    def tickets_events():
+        for entity in fetch_data(CRM_TICKETS_ENDPOINT, api_key=api_key):
+            yield _get_web_analytics_events("ticket", entity["hs_object_id"])
 
     @dlt.resource(write_disposition="replace")
     def tickets() -> Iterator[TDataItems]:
         yield fetch_data(CRM_TICKETS_ENDPOINT, api_key=api_key)
 
+    @dlt.resource(write_disposition="append")
+    def products_events():
+        for entity in fetch_data(CRM_PRODUCTS_ENDPOINT, api_key=api_key):
+            yield _get_web_analytics_events("product", entity["hs_object_id"])
+
     @dlt.resource(write_disposition="replace")
     def products() -> Iterator[TDataItems]:
         yield fetch_data(CRM_PRODUCTS_ENDPOINT, api_key=api_key)
+
+    @dlt.resource(write_disposition="append")
+    def quotes_events():
+        for entity in fetch_data(CRM_QUOTES_ENDPOINT, api_key=api_key):
+            yield _get_web_analytics_events("quote", entity["hs_object_id"])
 
     @dlt.resource(write_disposition="replace")
     def quotes() -> Iterator[TDataItems]:
         yield fetch_data(CRM_QUOTES_ENDPOINT, api_key=api_key)
 
-    @dlt.resource(write_disposition="replace")
-    def web_analytics_events() -> Iterator[TDataItems]:
-        yield fetch_data(WEB_ANALYTICS_EVENTS_ENDPOINT, api_key=api_key)
-
-    return companies(), contacts(), deals(), tickets(), products(), quotes(), web_analytics_events()
+    return companies(), companies_events(), \
+        contacts(), contacts_events(), \
+        deals(), deals_events(), \
+        tickets(), tickets_events(), \
+        products(), products_events(), \
+        quotes(), quotes_events()
