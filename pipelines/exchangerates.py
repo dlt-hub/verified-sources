5b4f5eba1f036737b36e8300062748b0426cffa1
mike-b <mikhail.bolshakov@coachhub.com>
2023-04-16 23:12:25 +0200

    ref(exchangfe_rates): change the way last updated is computed

--- pipelines/exchangerates.py
+++ pipelines/exchangerates.py
@@ -4,25 +4,6 @@ from exchange_rates import exchangerates_source
 CURRENCY_LIST = ["AUD", "BRL", "CAD", "CHF", "CNY", "DKK", "EUR", "GBP"]
 BASE_CURRENCY = "EUR"
 
-pipeline = dlt.pipeline(
-        pipeline_name="exchangerates", destination="postgres", dataset_name="exchangerates_data"
-    )
-
-def get_last_updated_at(default="2023-03-17T00:00:00Z"):
-    last_updated_query = (
-        f"select max(date)::date from exchangerates_data.exchangerates_resource"
-    )
-    # Query the database to get the most recent date that the conversion rates were loaded
-
-    with pipeline.sql_client() as client:
-        res = client.execute_sql(last_updated_query)
-        # todo: error handling - we would need to make sure this only happens for a "table not found" error.
-        if not res[0][0]:
-            last_val = dlt.current.state().setdefault("date", default)
-        else:
-            last_val = dlt.current.state().setdefault("date", res[0][0])
-
-    return last_val
 
 def load_euro_conversion_rates_incrementally(currency_list, base_currency):
     """
@@ -31,12 +12,13 @@ def load_euro_conversion_rates_incrementally(currency_list, base_currency):
     Args:
         currency_list (list): A list of currency codes to load the conversion rates for.
     """
-
-    # Define a pipeline to load the data into the PostgreSQL database
-    last_updated_at = get_last_updated_at()
-    load_info = pipeline.run(
-        exchangerates_source(currency_list, last_updated_at, base_currency)
+    pipeline = dlt.pipeline(
+        pipeline_name="exchangerates",
+        destination="postgres",
+        dataset_name="exchangerates_data",
     )
+    # Define a pipeline to load the data into the PostgreSQL database
+    load_info = pipeline.run(exchangerates_source(currency_list, base_currency))
 
     print(load_info)
 
