fc594d176c9d5ce97364a5627bc2f2c65afe5d87
teomeskuti <teomeskuti29@gmail.com>
2023-04-17 11:25:32 +0200

    Created very basic pipeline for Matomo

--- pipelines/matomo/__init__.py
+++ pipelines/matomo/__init__.py
@@ -8,38 +8,38 @@ from .helpers.matomo_client import MatomoAPIClient
 
 @dlt.source(max_table_nesting=2)
 def matomo(credentials: Dict[str, str] = dlt.secrets.value, id_site: int = dlt.config.value,
-           period: str = dlt.config.value, date: str = dlt.config.value) -> List[DltResource]:
+           period: str = dlt.config.value, date: str = dlt.config.value, method: str = dlt.config.value) -> List[DltResource]:
     """
 
     :param credentials:
     :param id_site:
     :param period:
     :param date:
-    :param methods:
+    :param method:
     :return:
     """
 
     # Create an instance of the Matomo API client
     matomo_client = MatomoAPIClient(base_url=credentials["url"], auth_token=credentials["api_token"])
-    reports = get_reports(matomo_client=matomo_client, id_site=id_site, period=period, date=date)
-    metadata = get_metadata(matomo_client=matomo_client, id_site=id_site, period=period, date=date)
-    return [reports, metadata]
+    reports = get_reports(matomo_client=matomo_client, id_site=id_site, period=period, date=date, method=method)
+    metadata = get_metadata(matomo_client=matomo_client, id_site=id_site, period=period, date=date, method=method)
+    return [metadata, reports]
 
 
-@dlt.resource(write_disposition="replace", name="reports")
-def get_reports(matomo_client: MatomoAPIClient,  id_site: int = dlt.config.value, period: str = dlt.config.value,
-                date: str = dlt.config.value) -> Iterator[TDataItem]:
+@dlt.resource(write_disposition="append", name="reports")
+def get_reports(matomo_client: MatomoAPIClient, id_site: int = dlt.config.value, period: str = dlt.config.value,
+                date: str = dlt.config.value, method: str = dlt.config.value) -> Iterator[TDataItem]:
     """
 
+    :param method:
     :param matomo_client:
     :param id_site:
     :param period:
     :param date:
-    :param methods:
     :return:
     """
     # Get the metadata for the available reports
-    reports = matomo_client.get_visits(id_site=id_site, period=period, date=date)
+    reports = matomo_client.get_matomo_data(site_id=id_site, period=period, date=date, api_method=method)
 
     # Print the metadata
     for report in reports:
@@ -47,19 +47,19 @@ def get_reports(matomo_client: MatomoAPIClient,  id_site: int = dlt.config.value
 
 
 @dlt.resource(write_disposition="replace", name="metadata")
-def get_metadata(matomo_client: MatomoAPIClient,  id_site: int = dlt.config.value, period: str = dlt.config.value,
-                 date: str = dlt.config.value) -> Iterator[TDataItem]:
+def get_metadata(matomo_client: MatomoAPIClient, id_site: int = dlt.config.value, period: str = dlt.config.value,
+                 date: str = dlt.config.value, method: str = dlt.config.value) -> Iterator[TDataItem]:
     """
 
+    :param method:
     :param matomo_client:
     :param id_site:
     :param period:
     :param date:
-    :param methods:
     :return:
     """
     # Get the metadata for the available reports
-    reports = matomo_client.get_metadata(id_site=id_site, period=period, date=date)
+    reports = matomo_client.get_matomo_data(period=period, date=date, site_id=id_site, api_method=method)
     # Print the metadata
     for report in reports:
         yield report
