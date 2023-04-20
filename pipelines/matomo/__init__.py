1710285a5b41438177858ca6a8df29bb497b249f
teomeskuti <teomeskuti29@gmail.com>
2023-04-20 18:30:09 +0200

    Testing incomplete incremental load

--- pipelines/matomo/__init__.py
+++ pipelines/matomo/__init__.py
@@ -2,10 +2,13 @@
 import logging
 from typing import Dict, Iterator, List
 import dlt
+import pendulum
 from dlt.common import logger
 from dlt.common.typing import DictStrAny, TDataItem, TDataItems
 from dlt.extract.source import DltResource
 from .helpers.matomo_client import MatomoAPIClient
+
+FIRST_DAY_OF_MILLENNIUM = "2000-01-01"
 
 
 @dlt.source(max_table_nesting=2)
@@ -19,36 +22,57 @@ def matomo(credentials: Dict[str, str] = dlt.secrets.value, queries: List[DictSt
 
     # Create an instance of the Matomo API client
     client = MatomoAPIClient(base_url=credentials["url"], auth_token=credentials["api_token"])
-    resource_list = []
+    stuff = get_triggered()
+    resource_list = [stuff]
     for query in queries:
-        batch_data = get_data_batch(client=client, query=query, filter_limit=filter_limit)
-        resource_list.extend(batch_data)
+        name = query.get("resource_name")
+        methods = query.get("methods", [])
+        extra_params = query.get("extra_params", {})
+        period = query.get("period", "day")
+        site_id = query.get("site_id", 2)
+        filter_limit = filter_limit
+        batch_data = dlt.resource(get_data_batch(client=client,
+                                                 query=query,
+                                                 filter_limit=filter_limit),
+                                  name=name)
+        for method_data, method in zip(batch_data, methods):
+            method_resource = dlt.resource(process_report(method_data), name=f"{name}_{method}", write_disposition="append")
+            resource_list.append(method_resource)
     return resource_list
 
 
 def get_data_batch(client: MatomoAPIClient, query: DictStrAny, filter_limit: int) -> List[DltResource]:
     """
-    Get all the data in batches.
+    Get data in batches
     :param client:
     :param query:
+    :param filter_limit:
     :return:
     """
 
     name = query.get("resource_name")
-    date = query.get("date", "previous1000")
     extra_params = query.get("extra_params", {})
     methods = query.get("methods", [])
-    period = query.get("period", "date")
+    period = query.get("period", "day")
     site_ids = query.get("site_id", 2)
     filter_limit = filter_limit
-    # Get the metadata for the available reports
-    reports = client.get_query(date=date, extra_params=extra_params, methods=methods, period=period, site_id=site_ids, filter_limit=filter_limit)
-    resource_list = []
+
+    # configure incremental loading. start_date prio: last dlt load -> set start time -> 2000-01-01
+    # grab the start time from last dlt load if not filled, if that is also empty then use the first day of the millennium as the start time instead
+    if f"last_load_{name}" in dlt.state():
+        # take next day after yesterday to avoid double loads
+        start_date = pendulum.parse(dlt.state()[f"last_load_{name}"]).to_date_string()
+        if "date" in query:
+            logger.warning(f"Using the starting date: {start_date} for incremental report: {name} and ignoring start date passed as argument {query['date']}")
+    else:
+        start_date = query.get("date", FIRST_DAY_OF_MILLENNIUM)
+    date_range = f"{start_date},yesterday"
+    # Get data for all queries
+    reports = client.get_query(date=date_range, extra_params=extra_params, methods=methods, period=period, site_id=site_ids, filter_limit=filter_limit)
     for report in reports:
-        for method_data, method in zip(report, methods):
-            method_resource = dlt.resource(process_report(method_data), name=f"{name}_{method}", write_disposition="append")
-            resource_list.append(method_resource)
-    return resource_list
+        yield report
+    # save last_load in dlt state
+    dlt.state()[f"last_load_{name}"] = pendulum.yesterday().to_date_string()
 
 
 def process_report(report: Iterator[TDataItem]) -> Iterator[TDataItem]:
@@ -57,7 +81,6 @@ def process_report(report: Iterator[TDataItem]) -> Iterator[TDataItem]:
     :param report:
     :return:
     """
-
     if isinstance(report, dict):
         for key, value in report.items():
             # TODO: better way of checking for this
