bf2aedfb53a15dc5344262f2c20b41dc4774aa10
AstrakhantsevaAA <astra92293@gmail.com>
2023-04-27 12:04:24 +0200

    [feature] take only past 30 days from events table

--- pipelines/stripe_analytics_pipeline.py
+++ pipelines/stripe_analytics_pipeline.py
@@ -1,5 +1,8 @@
+from datetime import datetime, timedelta
 import dlt
-from stripe_analytics.stripe_analytics import calculate_mrr, churn_rate, stripe_source
+from stripe_analytics.stripe_analytics import stripe_source
+from stripe_analytics.metrics import calculate_mrr, churn_rate
+
 
 if __name__ == "__main__":
     pipeline = dlt.pipeline(
@@ -8,8 +11,6 @@ if __name__ == "__main__":
         dataset_name="stripe_customers_subscriptions",
     )
     source = stripe_source(limit=100, get_all_data=True)
-    print(source.resources.keys())
-    print(source.resources.selected.keys())
 
     load_info = pipeline.run(source)
     print(load_info)
@@ -18,8 +19,10 @@ if __name__ == "__main__":
         with client.execute_query("SELECT * FROM subscription") as table:
             sub_info = table.df()
 
+    # Access to events through the Retrieve Event API is guaranteed only for 30 days.
+    # But we probably have old data in the database.
     with pipeline.sql_client() as client:
-        with client.execute_query("SELECT * FROM event") as table:
+        with client.execute_query("SELECT * FROM event WHERE created > %s", datetime.now() - timedelta(30)) as table:
             event_info = table.df()
 
     mrr = calculate_mrr(sub_info)
