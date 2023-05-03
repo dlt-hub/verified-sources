987dc918632e7b5fd02a435d7c1a5613dcc1755e
Alena Astrakhantseva <astra92293@gmail.com>
2023-05-03 20:32:21 +0200

    fix imports of sources and resources

--- pipelines/stripe_analytics/__init__.py
+++ pipelines/stripe_analytics/__init__.py
@@ -1,3 +1,4 @@
 """
 This pipeline uses Stripe API and dlt to load data such as Customer, Subscription, Event and etc. to the database and to calculate the MRR and churn rate.
 """
+from .stripe_analytics import stripe_source, metrics_resource
