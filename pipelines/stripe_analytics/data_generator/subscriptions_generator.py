02e76cb39ee8efc1175a0c2b9ac17db8cc5f3a94
AstrakhantsevaAA <astra92293@gmail.com>
2023-05-03 13:22:08 +0200

    [feature] assign new subscriptions only on new customers + refactoring

--- pipelines/stripe_analytics/data_generator/subscriptions_generator.py
+++ pipelines/stripe_analytics/data_generator/subscriptions_generator.py
@@ -19,75 +19,77 @@ class StripeDataUploader:
             "pm_card_mastercard_prepaid",
             "pm_card_unionpay",
         ]
+        self.customers = []
+        self.plans = []
+        self.coupons = [None]
+        self.subscriptions = []
 
-    @staticmethod
-    def create_subscription(names: Tuple[str, ...], amounts: Tuple[str, ...]):
-        for name, amount in tqdm(zip(names, amounts), desc="Create subscriptions..."):
+    def create_subscription(self, names: Tuple[str, ...]):
+        for name in tqdm(names, desc="Create subscriptions..."):
+            amount = str(random.randint(100, 10000))
+
             starter_subscription = stripe.Product.create(
                 name=f"Starter {name} Subscription",
                 description=f"${round(float(amount)/100, 2)}/Month for {name} subscription",
             )
 
-            stripe.Price.create(
+            plan = stripe.Price.create(
                 unit_amount=amount,
                 currency="usd",
-                recurring={"interval": "month"},
+                recurring={"interval": random.choice(["month", "year"])},
                 product=starter_subscription["id"],
             )
+
+            self.plans.append(plan.id)
 
     def create_customer(self, names: Tuple[str, ...]):
         for name in tqdm(names, desc="Create customers..."):
             splitted_name = name.split()
             card_picked = random.choice(self.card_types)
 
-            stripe.Customer.create(
+            customer = stripe.Customer.create(
                 name=name,
                 email=f"{splitted_name[0]}_{splitted_name[1]}@example.com",
                 payment_method=card_picked,
                 invoice_settings={"default_payment_method": card_picked},
             )
 
-    @staticmethod
-    def create_coupons():
-        stripe.Coupon.create(
+            self.customers.append(customer)
+
+    def create_coupons(self):
+        coupon1 = stripe.Coupon.create(
             duration="once",
             id=f"free-period_{random.randint(0, 1000000)}",
             percent_off=100,
         )
-        stripe.Coupon.create(
+        coupon2 = stripe.Coupon.create(
             duration="forever",
             id=f"discount_{random.randint(0, 1000000)}",
             percent_off=15,
         )
 
-    @staticmethod
-    def attach_customer_and_subscription():
-        all_customers = stripe.Customer.list()["data"]
-        all_subscriptions = stripe.Price.list()["data"]
-        all_coupons = [coupon["id"] for coupon in stripe.Coupon.list()["data"]]
-        all_coupons.append(None)
+        self.coupons.extend([coupon1.id, coupon2.id])
 
+    def attach_customer_and_subscription(self):
         for sample_customer in tqdm(
-            all_customers, desc="Randomly assign subscriptions on customers..."
+            self.customers, desc="Randomly assign subscriptions on customers..."
         ):
-            stripe.Subscription.create(
-                customer=sample_customer["id"],
-                items=[{"price": random.choice(all_subscriptions)["id"]}],
-                default_payment_method=sample_customer["invoice_settings"].get(
-                    "default_payment_method", "pm_card_visa"
-                ),
-                coupon=random.choice(all_coupons),
+            sub = stripe.Subscription.create(
+                customer=sample_customer.id,
+                items=[{"price": random.choice(self.plans)}],
+                default_payment_method=sample_customer.invoice_settings.default_payment_method,
+                coupon=random.choice(self.coupons),
             )
 
-    @staticmethod
-    def randomly_delete_subscriptions(number: int = 5):
-        all_customers_subscriptions = stripe.Subscription.list()["data"]
-        to_delete = random.sample(all_customers_subscriptions, k=number)
+            self.subscriptions.append(sub)
+
+    def randomly_delete_subscriptions(self, number: int = 5):
+        to_delete = random.sample(self.subscriptions, k=number)
 
         for sample in tqdm(
             to_delete, desc="Randomly delete some customer's subscriptions..."
         ):
-            stripe.Subscription.delete(sample["id"])
+            stripe.Subscription.delete(sample.id)
 
 
 if __name__ == "__main__":
@@ -96,11 +98,10 @@ if __name__ == "__main__":
     generator = RandomDataGenerator()
 
     rand_plans = generator.random_plans_generation(2)
-    rand_amounts = generator.random_amounts_generation(2)
-    rand_customers = generator.random_names_generation(20)
+    rand_customers = generator.random_names_generation(50)
 
-    uploader.create_subscription(rand_plans, rand_amounts)
+    uploader.create_subscription(rand_plans)
     uploader.create_customer(rand_customers)
     uploader.create_coupons()
     uploader.attach_customer_and_subscription()
-    uploader.randomly_delete_subscriptions(5)
+    uploader.randomly_delete_subscriptions(10)
