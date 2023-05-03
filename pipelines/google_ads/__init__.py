116999679f13509c6fab200fc7e4790997f62edf
redicane <caneredi@gmail.com>
2023-05-03 14:57:41 +0200

    Stubs for sources, resources and tests. General structure for the project

--- pipelines/google_ads/__init__.py
+++ pipelines/google_ads/__init__.py
@@ -1,39 +1,24 @@
-"""This example illustrates how to get all campaigns.
-
-To add campaigns, run add_campaigns.py.
-"""
-
-from google.ads.googleads.client import GoogleAdsClient
+from typing import Dict, List
+import dlt
+from dlt.common.typing import DictStrAny, DictStrStr, TDataItem
+from dlt.common.pendulum import pendulum
 
 
-def main(client, customer_id):
-    ga_service = client.get_service("GoogleAdsService")
-
-    query = """
-        SELECT
-          campaign.id,
-          campaign.name
-        FROM campaign
-        ORDER BY campaign.id"""
-
-    # Issues a search request using streaming.
-    stream = ga_service.search_stream(customer_id=customer_id, query=query)
-
-    for batch in stream:
-        for row in batch.results:
-            print(
-                f"Campaign with ID {row.campaign.id} and name "
-                f'"{row.campaign.name}" was found.'
-            )
+@dlt.source(max_table_nesting=2)
+def google_ads_query(credentials=dlt.secrets.value, query_list: List[DictStrAny] = dlt.config.value):
+    pass
 
 
-if __name__ == "__main__":
+@dlt.source(max_table_nesting=2)
+def google_ads():
+    pass
 
-    credentials = {
-        "developer_token": "",
-        "refresh_token": "",
-        "client_id": "",
-        "client_secret": ""}
 
-    client = GoogleAdsClient.load_from_dict(config_dict=credentials)
-    main(client=client, customer_id="")
+@dlt.resource(name="dimension_tables", write_disposition="replace")
+def get_dimensions():
+    pass
+
+
+@dlt.resource(name="reports_table", write_disposition="append")
+def get_reports():
+    pass
