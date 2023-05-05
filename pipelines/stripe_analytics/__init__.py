98ef9dc9912189fce9a15e53186b476ad21cecc3
AstrakhantsevaAA <astra92293@gmail.com>
2023-05-05 11:48:23 +0200

    trying fix init test

--- pipelines/stripe_analytics/__init__.py
+++ pipelines/stripe_analytics/__init__.py
@@ -1,4 +1,3 @@
-"""
-This pipeline uses Stripe API and dlt to load data such as Customer, Subscription, Event and etc. to the database and to calculate the MRR and churn rate.
-"""
+""" This pipeline uses Stripe API and dlt to load data such as Customer, Subscription, Event and etc. to the database and to calculate the MRR and churn rate. """
+
 from .stripe_analytics import stripe_source, metrics_resource
