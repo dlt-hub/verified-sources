a68b8ea21468e55691ea00247142a95f9d024332
AstrakhantsevaAA <astra92293@gmail.com>
2023-05-09 15:35:29 +0200

    add fetch

--- pipelines/workable/__init__.py
+++ pipelines/workable/__init__.py
@@ -1,4 +1,5 @@
-from typing import Any, Dict, Generator, Optional
+from typing import Any, Union, Generator, Optional
+import logging
 
 import requests
 import dlt
@@ -6,13 +7,34 @@ from pendulum import DateTime, datetime, parse
 
 
 @dlt.source(name="workable")
-def workable_source(access_token=dlt.secrets.value, config=dlt.config.value):
+def workable_source(access_token=dlt.secrets.value, config=dlt.config.value, start_date: Optional[DateTime] = None, fetch: bool = False,):
     headers = {
         "accept": "application/json",
         "Authorization": f"Bearer {access_token}"
     }
+    url = config["subdomain_url"]
+    start_date_unix = start_date.isoformat() if start_date is not None else datetime(2000, 1, 1).isoformat()
 
-    return candidates_resource(headers, config)
+    if fetch:
+        @dlt.resource(write_disposition="merge", primary_key="id")
+        def fetch_candidates_resource(
+            updated_at: Optional[Any] = dlt.sources.incremental("updated_at", initial_value=start_date_unix)
+        ) -> list:
+            logging.warning("Fetching data by 'updated_at'. Loading modified and new data...")
+            params = {"updated_after": updated_at.last_value}
+            yield dlt.mark.with_table_name(pagination(url, "candidates", headers, params), "candidates")
+
+        return fetch_candidates_resource()
+    else:
+        @dlt.resource(write_disposition="append", primary_key="id")
+        def load_candidates_resource(
+            created_at: Optional[Any] = dlt.sources.incremental("created_at", initial_value=start_date_unix),
+        ) -> list:
+            logging.warning("'fetch' is False. Loading only new data by 'created_at'...")
+            params = {"created_after": created_at.last_value}
+            yield dlt.mark.with_table_name(pagination(url, "candidates", headers, params), "candidates")
+
+        return load_candidates_resource()
 
 
 @dlt.resource
@@ -25,28 +47,29 @@ def workable_events(headers: dict, config: dict) -> list:
     yield response.json()["events"]
 
 
-@dlt.resource(name="candidates", write_disposition="merge", primary_key="id")
-def candidates_resource(
-    headers: dict,
-    config: dict,
-    updated_at: Optional[Any] = dlt.sources.incremental("updated_at", initial_value=datetime(2000, 1, 1))
-) -> list:
-    url = f"{config['subdomain_url']}/candidates"
+def pagination(url: str, endpoint: str, headers: dict, params: dict) -> Generator:
+    url = f"{url}/{endpoint}"
 
     has_more = True
-
     while has_more:
-
-        response = requests.get(url, headers=headers, params={"updated_after": updated_at.last_value})
+        response = requests.get(url, headers=headers, params=params)
         response_json = response.json()
         paging = response_json.get("paging")
+
         if paging is not None:
             url = paging.get("next")
         else:
             has_more = False
 
-        response_with_data = []
-        for resp in response_json["candidates"]:
-            response_with_data.append({k: (v if k != "updated_at" else parse(v)) for k, v in resp.items()})
+        yield response_json[endpoint]
 
-        yield response_with_data
+
+def transform_date(date: Union[str, DateTime, int]) -> int:
+    if isinstance(date, str):
+        date = parse(date)
+    if isinstance(date, DateTime):
+        # convert to unix timestamp
+        date = int(date.timestamp())
+    return date
+
+
--- pipelines/workable_pipeline.py
+++ pipelines/workable_pipeline.py
@@ -6,6 +6,6 @@ from workable import workable_source
 if __name__ == "__main__":
     pipeline = dlt.pipeline(pipeline_name='workable', destination='duckdb', dataset_name='workable_data')
     # run the pipeline with your parameters
-    load_info = pipeline.run(workable_source())
+    load_info = pipeline.run(workable_source(fetch=True))
     # pretty print the information on data that was loaded
     print(load_info)
