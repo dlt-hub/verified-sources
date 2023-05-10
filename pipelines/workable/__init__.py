7206594e1d1568d5f8be9f6caf8972fd8d41affa
AstrakhantsevaAA <astra92293@gmail.com>
2023-05-10 14:31:01 +0200

    created 3 sources for 3 use cases

--- pipelines/workable/__init__.py
+++ pipelines/workable/__init__.py
@@ -1,32 +1,91 @@
-from typing import Any, Optional
 import logging
+from typing import Any, Generator, Optional
 
 import dlt
-from pendulum import DateTime, datetime
-from .helpers import pagination
+from pendulum import DateTime
+
+from .workable_client import WorkableClient
+
+logging.basicConfig(format="%(asctime)s - %(message)s", level=logging.INFO)
 
 
-@dlt.source(name="workable")
-def workable_source(access_token=dlt.secrets.value, config=dlt.config.value, start_date: Optional[DateTime] = None,):
-    headers = {
-        "accept": "application/json",
-        "Authorization": f"Bearer {access_token}"
-    }
-    url = config["subdomain_url"]
-    start_date_unix = start_date.isoformat() if start_date is not None else datetime(2000, 1, 1).isoformat()
+@dlt.source
+def workable_load_data(
+    endpoints: tuple = ("members", "jobs"),
+    credentials=dlt.secrets.value,
+    start_date: Optional[DateTime] = None,
+):
+    workable = WorkableClient(
+        credentials["access_token"], credentials["subdomain"], start_date=start_date
+    )
+    params = {"created_after": workable.start_date_iso}
+
+    def load_data(endpoint: str) -> Generator[list, Any, None]:
+        logging.info(
+            f"Loading all data from '{endpoint}' by 'created_at' in 'replace' mode..."
+        )
+        yield workable.pagination(endpoint=endpoint, params=params)
+
+    for endpoint in endpoints:
+        yield dlt.resource(
+            load_data,
+            name=endpoint,
+            write_disposition="replace",
+        )(endpoint)
+
+
+@dlt.source
+def workable_candidates(
+    credentials=dlt.secrets.value,
+    start_date: Optional[DateTime] = None,
+):
+    workable = WorkableClient(
+        credentials["access_token"], credentials["subdomain"], start_date=start_date
+    )
 
     @dlt.resource(name="candidates", write_disposition="merge", primary_key="id")
     def fetch_candidates_resource(
-        updated_at: Optional[Any] = dlt.sources.incremental("updated_at", initial_value=start_date_unix)
+        updated_at: Optional[Any] = dlt.sources.incremental(
+            "updated_at", initial_value=workable.start_date_iso
+        )
     ) -> list:
-        logging.warning("Fetching data by 'updated_at'. Loading modified and new data...")
-        params = {"updated_after": updated_at.last_value}
-        yield pagination(url, "candidates", headers, params)
-
-    @dlt.resource(name="jobs", write_disposition="merge", primary_key="id")
-    def jobs_resource(
-    ) -> list:
-        yield pagination(url, "jobs", headers, {})
+        logging.info(
+            "Fetching data from 'candidates' by 'updated_at'. Loading modified and new data..."
+        )
+        yield workable.pagination(
+            endpoint="candidates", params={"updated_after": updated_at.last_value}
+        )
 
     return fetch_candidates_resource()
 
+
+@dlt.source
+def workable_jobs_activities(
+    credentials=dlt.secrets.value,
+    start_date: Optional[DateTime] = None,
+):
+    workable = WorkableClient(
+        credentials["access_token"], credentials["subdomain"], start_date=start_date
+    )
+
+    @dlt.resource(name="jobs", write_disposition="replace", primary_key="id")
+    def jobs_resource():
+        yield workable.pagination(
+            "jobs", params={"created_after": workable.start_date_iso}
+        )
+
+    def _get_activities(url: str, shortcode: str):
+        url_with_shortcode = f"{url}/jobs/{shortcode}"
+
+        yield workable.pagination(
+            "activities",
+            custom_url=url_with_shortcode,
+            params={"created_after": workable.start_date_iso},
+        )
+
+    @dlt.transformer(data_from=jobs_resource)
+    def jobs_activities(job_item):
+        for job in job_item:
+            yield _get_activities(workable.base_url, job["shortcode"])
+
+    return jobs_activities()
--- pipelines/workable_pipeline.py
+++ pipelines/workable_pipeline.py
@@ -1,11 +1,49 @@
 import dlt
+from workable import workable_candidates, workable_jobs_activities, workable_load_data
 
-from workable import workable_source, loaded
+
+def load_all_data(
+    endpoints: tuple = ("members", "jobs", "stages", "candidates", "events")
+):
+    pipeline = dlt.pipeline(
+        pipeline_name="workable",
+        destination="duckdb",
+        dataset_name="workable_all_data",
+    )
+    load_data = workable_load_data(endpoints=endpoints)
+    # run the pipeline with your parameters
+    load_info = pipeline.run(load_data)
+    # pretty print the information on data that was loaded
+    print(load_info)
+
+
+def load_incremental_candidates():
+    pipeline = dlt.pipeline(
+        pipeline_name="workable_incremental",
+        destination="duckdb",
+        dataset_name="workable_candidates",
+    )
+    candidates = workable_candidates()
+    # run the pipeline with your parameters
+    load_info = pipeline.run(candidates)
+    # pretty print the information on data that was loaded
+    print(load_info)
+
+
+def load_jobs_activities():
+    pipeline = dlt.pipeline(
+        pipeline_name="workable_ja",
+        destination="duckdb",
+        dataset_name="workable_activities",
+    )
+    job_activities = workable_jobs_activities()
+    # run the pipeline with your parameters
+    load_info = pipeline.run(job_activities)
+    # pretty print the information on data that was loaded
+    print(load_info)
 
 
 if __name__ == "__main__":
-    pipeline = dlt.pipeline(pipeline_name='workable', destination='duckdb', dataset_name='workable_data')
-    # run the pipeline with your parameters
-    load_info = pipeline.run(loaded())
-    # pretty print the information on data that was loaded
-    print(load_info)
+    load_all_data()
+    load_incremental_candidates()
+    load_jobs_activities()
