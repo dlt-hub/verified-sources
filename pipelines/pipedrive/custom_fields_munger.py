b51a7b7263a4cfa3c5c8ef651cfa1aacda1b987a
Steinthor Palsson <steini90@gmail.com>
2023-05-09 20:15:11 -0400

    Map pipedrive custom field enum values to labels

--- pipelines/pipedrive/custom_fields_munger.py
+++ pipelines/pipedrive/custom_fields_munger.py
@@ -1,8 +1,15 @@
-from typing import Any, Dict, Iterable, Iterator
+from typing import Any, Dict, Iterable, Iterator, TypedDict, Optional
 
 import dlt
 
 from .typing import TDataPage
+
+
+
+class TFieldMapping(TypedDict):
+    name: str
+    normalized_name: str
+    options: Optional[Dict[str, str]]
 
 
 def update_fields_mapping(new_fields_mapping: TDataPage, existing_fields_mapping: Dict[str, Any]) -> Dict[str, Any]:
@@ -17,35 +24,56 @@ def update_fields_mapping(new_fields_mapping: TDataPage, existing_fields_mapping
             # We assume that pipedrive's hash strings are meant to be an univoque representation of custom fields' name, so dlt's state shouldn't be updated while those values
             # remain unchanged
             # First of all, we search on and update dlt's state
-            if existing_fields_mapping:
-                if data_item['key'] not in existing_fields_mapping:
-                    data_item_mapping = _normalize_map(data_item)
-                    existing_fields_mapping.update(data_item_mapping)
-            else:
-                existing_fields_mapping = _normalize_map(data_item)
+            existing_fields_mapping = _update_field(data_item, existing_fields_mapping)
             # We end up updating data with dlt's state
             # data_item['key'] = existing_fields_mapping[data_item['key']]['normalized_name']
     return existing_fields_mapping
 
 
-def _normalize_map(data_item: Dict[str, Any]) -> Dict[str, Dict[str, str]]:
+def _update_field(data_item: Dict[str, Any], existing_fields_mapping: Optional[Dict[str, TFieldMapping]]) -> Dict[str, TFieldMapping]:
+    """Create or update the given field's info the custom fields state
+    If the field hash already exists in the state from previous runs the name is not updated.
+    New enum options (if any) are appended to the state.
+    """
+    existing_fields_mapping = existing_fields_mapping or {}
+    key = data_item['key']
+    options = data_item.get('options', [])
+    new_options_map = {str(o['id']): o['label'] for o in options}
+    existing_field = existing_fields_mapping.get(key)
+    if not existing_field:
+        existing_fields_mapping[key] = dict(
+            name=data_item['name'],
+            normalized_name=_normalized_name(data_item['name']),
+            options=new_options_map
+        )
+        return existing_fields_mapping
+    existing_options = existing_field.get('options', {})
+    if not existing_options or existing_options == new_options_map:
+        existing_field['options'] = new_options_map
+        return existing_fields_mapping
+    # Add new enum options to the existing options array
+    # so that when option is renamed the original label remains valid
+    new_option_keys = set(new_options_map) - set(existing_options)
+    for key in new_option_keys:
+        existing_options[key] = new_options_map[key]
+    existing_field['options'] = existing_options
+    return existing_fields_mapping
+
+
+def _normalized_name(name: str) -> str:
     source_schema = dlt.current.source_schema()
-    normalized_name = data_item['name'].strip()  # remove leading and trailing spaces
-    normalized_name = source_schema.naming.normalize_identifier(normalized_name)
-    return {
-        data_item['key']: {
-            'name': data_item['name'],
-            'normalized_name': normalized_name
-            }
-    }
+    normalized_name = name.strip()  # remove leading and trailing spaces
+    return source_schema.naming.normalize_identifier(normalized_name)
 
 
 def rename_fields(data: TDataPage, fields_mapping: Dict[str, Any]) -> TDataPage:
     if not fields_mapping:
         return data
-    renames = [(hash_string, names["name"]) for hash_string, names in fields_mapping.items()]
     for data_item in data:
-        for hash_string, name in renames:
+        for hash_string, field in fields_mapping.items():
             if hash_string in data_item:
-                data_item[name] = data_item.pop(hash_string)
+                field_value = data_item.pop(hash_string)
+                # Get label instead of ID for enum fields
+                options_map = field.get('options', {})
+                data_item[field['name']] = options_map.get(field_value, field_value)
     return data
