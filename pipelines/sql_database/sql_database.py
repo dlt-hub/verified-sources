8f61117a29a2671ca76249cdc93f6d50357f8a91
Steinthor Palsson <steini90@gmail.com>
2023-05-10 20:02:02 -0400

    Don't use nested resource in sql_table
    Fixes issue with incremental where source state is pulled before pipeline run

--- pipelines/sql_database/sql_database.py
+++ pipelines/sql_database/sql_database.py
@@ -13,18 +13,25 @@ from .util import table_rows, engine_from_credentials, get_primary_key
 
 
 @configspec
-class SqlTableConfiguration(BaseConfiguration):
+class SqlDatabaseTableConfiguration(BaseConfiguration):
     incremental: Optional[dlt.sources.incremental] = None  # type: ignore[type-arg]
 
 
-@dlt.resource
+@configspec
+class SqlTableResourceConfiguration(BaseConfiguration):
+    credentials: ConnectionStringCredentials
+    table: str
+    incremental: Optional[dlt.sources.incremental] = None  # type: ignore[type-arg]
+    schema: Optional[str]
+
+
+@dlt.common.configuration.with_config(sections=('sources', 'sql_database'), spec=SqlTableResourceConfiguration)
 def sql_table(
     credentials: Union[ConnectionStringCredentials, Engine, str] = dlt.secrets.value,
     table: str = dlt.config.value,
     schema: Optional[str] = dlt.config.value,
     metadata: Optional[MetaData] = None,
     incremental: Optional[dlt.sources.incremental[Any]] = None,
-    write_disposition: TWriteDisposition = 'append'
 ) -> DltResource:
     """A dlt resource which loads data from an SQL database table using SQLAlchemy.
 
@@ -35,7 +42,6 @@ def sql_table(
     :param incremental: Option to enable incremental loading for the table. E.g. `incremental=dlt.source.incremental('updated_at', pendulum.parse('2022-01-01T00:00:00Z'))`
     :param write_disposition: Write disposition of the resource
     """
-
     engine = engine_from_credentials(credentials)
     engine.execution_options(stream_results=True)
     metadata = metadata or MetaData(schema=schema)
@@ -43,7 +49,7 @@ def sql_table(
     table_obj = Table(table, metadata, autoload_with=engine)
 
     return dlt.resource(
-        table_rows, name=table_obj.name, write_disposition=write_disposition, primary_key=get_primary_key(table_obj)
+        table_rows, name=table_obj.name, primary_key=get_primary_key(table_obj)
     )(engine, table_obj, incremental=incremental)
 
 
@@ -82,7 +88,7 @@ def sql_database(
             table_rows,
             name=table.name,
             primary_key=get_primary_key(table),
-            spec=SqlTableConfiguration,
+            spec=SqlDatabaseTableConfiguration,
         )(engine, table)
         for table in tables
     ]
