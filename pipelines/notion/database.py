bbef9748be18cc09dedbb895185e1fe17844558a
burnash <anton.burnashev@gmail.com>
2023-05-11 14:16:59 +0300

    Add Notion pipeline

---
+++ pipelines/notion/database.py
@@ -0,0 +1,79 @@
+from typing import Dict, Optional, Any
+from .client import NotionClient
+
+
+class NotionDatabase:
+    """
+    A class to represent a Notion database.
+
+    Attributes:
+        database_id (str): The ID of the Notion database.
+        notion_client (NotionClient): A client to interact with the Notion API.
+    """
+
+    def __init__(self, database_id: str, notion_client: NotionClient):
+        self.database_id = database_id
+        self.notion_client = notion_client
+
+    def get_structure(self) -> Dict[str, Any]:
+        """Retrieves the structure of the database.
+
+        Notion API Reference. Retrieve a database:
+            https://developers.notion.com/reference/retrieve-a-database
+
+        Returns:
+            Dict[str, Any]: The structure of the database.
+        """
+        return self.notion_client.fetch_resource('databases', self.database_id)
+
+    def query(
+        self,
+        filter_properties: Optional[Dict[str, Any]] = None,
+        filter: Optional[Dict[str, Any]] = None,
+        sorts: Optional[Dict[str, Any]] = None,
+        start_cursor: Optional[str] = None,
+        page_size: Optional[int] = None,
+    ) -> Dict[str, Any]:
+        """Queries the database for records.
+
+        Notion API Reference. Query a database:
+            https://developers.notion.com/reference/post-database-query
+
+        Args:
+            filter_properties (Dict[str, Any], optional): A dictionary of
+                properties to filter the records by. Defaults to None.
+            filter (Dict[str, Any], optional): A dictionary of filters to
+                apply to the records. Defaults to None.
+            sorts (Dict[str, Any], optional): A dictionary of sorts to apply
+                to the records. Defaults to None.
+            start_cursor (str, optional): The cursor to start the query at.
+                Defaults to None.
+            page_size (int, optional): The number of records to return.
+                Defaults to None.
+
+        Yields:
+            Dict[str, Any]: A record from the database.
+        """
+        payload = {
+            'filter': filter,
+            'sorts': sorts,
+            'start_cursor': start_cursor,
+            'page_size': page_size,
+        }
+
+        has_more = True
+
+        while has_more:
+            response = self.notion_client.send_payload(
+                'databases',
+                self.database_id,
+                subresource='query',
+                query_params=filter_properties,
+                payload=payload,
+            )
+
+            for result in response.get('results', []):
+                yield result
+
+            has_more = response.get('has_more')
+            start_cursor = response.get('next_cursor')
