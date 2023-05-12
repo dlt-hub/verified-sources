e36d167418108569443ef61c59cba23e62c14d4c
AstrakhantsevaAA <astra92293@gmail.com>
2023-05-12 14:32:16 +0200

    added use case with fitering by date

--- pipelines/workable/__init__.py
+++ pipelines/workable/__init__.py
@@ -32,20 +32,21 @@ DEFAULT_ENDPOINTS = (
     "custom_attributes",
 )
 
-DEFAULT_CANDIDATES_DETAILS = (
-    "activities",
-    "offer",
-)
-
-DEFAULT_JOBS_DETAILS = (
-    "activities",
-    "application_form",
-    "questions",
-    "stages",
-    "custom_attributes",
-    "members",
-    "recruiters",
-)
+DEFAULT_DETAILS = {
+    "candidates": (
+        "activities",
+        "offer",
+    ),
+    "jobs": (
+        "activities",
+        "application_form",
+        "questions",
+        "stages",
+        "custom_attributes",
+        "members",
+        "recruiters",
+    )
+}
 
 
 @dlt.source(name="workable")
@@ -53,6 +54,7 @@ def workable_source(
     access_token: str = dlt.secrets.value,
     subdomain: str = dlt.config.value,
     start_date: Optional[DateTime] = None,
+    load_details: bool = False,
 ) -> Sequence[DltResource]:
     """
     Retrieves data from the Workable API for the specified endpoints (DEFAULT_ENDPOINTS + 'candidates').
@@ -60,20 +62,24 @@ def workable_source(
     so in most cases we are forced to load the date in 'replace' mode.
     'Сandidates' are the only endpoints that have a key 'updated_at', which means that we can update the data incrementally.
 
+    Resources that depend on another resource are implemented as transformers,
+    so they can re-use the original resource data without re-downloading.
+
     Parameters:
         access_token: The API access token for authentication. Defaults to the value in the `dlt.secrets` object.
         subdomain: The subdomain name for the Workable account. Defaults to the value in the `dlt.config` object.
         start_date: An optional start date to limit the data retrieved. Defaults to January 1, 2000.
-
+                    It does not affect dependent resources (jobs_activities, candidates_activities, etc).
+        load_details: A boolean flag enables loading data from endpoints that depend on the main endpoints.
     """
     resources = {}
     workable = WorkableClient(access_token, subdomain, start_date=start_date)
     params = {"created_after": workable.start_date_iso}
 
-    # This resource is suitable for all types of endpoints, including 'customers',
-    # but endpoint 'customers' can also be loaded in incremental mode (see candidates_resource).
+    # This resource is suitable for all types of endpoints, including 'candidates',
+    # but endpoint 'candidates' can also be loaded in incremental mode (see candidates_resource).
     def load_data(endpoint: str) -> Generator[list, Any, None]:
-        logging.info(
+        logging.debug(
             f"Loading all data from '{endpoint}' by 'created_at' in 'replace' mode..."
         )
         yield workable.pagination(endpoint=endpoint, params=params)
@@ -97,26 +103,27 @@ def workable_source(
         The 'updated_at' parameter is managed by the dlt.sources.incremental method.
         This function is suitable only for the 'candidates' endpoint in incremental mode.
         """
-        logging.info("Fetching data from 'candidates' by 'updated_at'. Loading modified and new data...")
+        logging.debug("Fetching data from 'candidates' by 'updated_at'. Loading modified and new data...")
         yield workable.pagination(endpoint="candidates", params={"updated_after": updated_at.last_value})
 
     yield candidates_resource
 
-    def _get_details(page, main_endpoint, sub_endpoint_name: str, code_key: str):
-        for item in page:
-            yield workable.details_from_endpoint(main_endpoint, item[code_key], sub_endpoint_name)
+    if load_details:
+        def _get_details(page, main_endpoint, sub_endpoint_name: str, code_key: str):
+            for item in page:
+                yield workable.details_from_endpoint(main_endpoint, item[code_key], sub_endpoint_name)
 
-    # A transformer functions that yield the activities etc.(DEFAULT_JOBS_DETAILS) for each job.
-    for sub_endpoint in DEFAULT_JOBS_DETAILS:
-        yield resources["jobs"] | dlt.transformer(
-            name=f'jobs_{sub_endpoint}', write_disposition='replace'
-        )(_get_details)("jobs", sub_endpoint, "shortcode")
+        # A transformer functions that yield the activities, questions, etc. for each job.
+        for sub_endpoint in DEFAULT_DETAILS["jobs"]:
+            yield resources["jobs"] | dlt.transformer(
+                name=f'jobs_{sub_endpoint}', write_disposition='replace'
+            )(_get_details)("jobs", sub_endpoint, "shortcode")
 
-    # A transformer functions that yield the activities and offers for each candidate.
-    for sub_endpoint in DEFAULT_CANDIDATES_DETAILS:
-        yield candidates_resource | dlt.transformer(
-            name=f'candidates_{sub_endpoint}', write_disposition='append'
-        )(_get_details)("candidates", sub_endpoint, "id")
+        # A transformer functions that yield the activities and offers for each candidate.
+        for sub_endpoint in DEFAULT_DETAILS["candidates"]:
+            yield candidates_resource | dlt.transformer(
+                name=f'candidates_{sub_endpoint}', write_disposition='append'
+            )(_get_details)("candidates", sub_endpoint, "id")
 
 
 #TODO: add Events https://workable.readme.io/reference/events
--- pipelines/workable_pipeline.py
+++ pipelines/workable_pipeline.py
@@ -1,4 +1,5 @@
 import dlt
+from pendulum import datetime
 
 from workable import (
     workable_source
@@ -19,6 +20,35 @@ def load_all_data(
     print(load_info)
 
 
-if __name__ == "__main__":
-    load_all_data()
+def load_all_data_with_details(
+):
+    pipeline = dlt.pipeline(
+        pipeline_name="workable",
+        destination="duckdb",
+        dataset_name="workable_all_data",
+    )
+    load_data = workable_source(load_details=True)
+    # run the pipeline with your parameters
+    load_info = pipeline.run(load_data)
+    # pretty print the information on data that was loaded
+    print(load_info)
 
+
+def load_data_by_date(
+):
+    pipeline = dlt.pipeline(
+        pipeline_name="workable",
+        destination="duckdb",
+        dataset_name="workable_all_data",
+    )
+    load_data = workable_source(start_date=datetime(2023, 2, 1), load_details=True)
+    # run the pipeline with your parameters
+    load_info = pipeline.run(load_data)
+    # pretty print the information on data that was loaded
+    print(load_info)
+
+
+if __name__ == "__main__":
+    # load_all_data()
+    # load_all_data_with_details()
+    load_data_by_date()
