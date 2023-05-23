e861f446b9f9192bc73bca00c77e64a5fa2c15d8
Dave <shrps@posteo.net>
2023-05-23 11:11:51 +0200

    rewrite pipeline and extend tests

--- pipelines/salesforce/__init__.py
+++ pipelines/salesforce/__init__.py
@@ -10,6 +10,7 @@ To get the security token: https://onlinehelp.coveo.com/en/ces/7.0/administrator
 import typing as t
 import pendulum
 
+from typing import Any, Dict, Iterator, Iterator, Optional
 
 import dlt
 from simple_salesforce import Salesforce
@@ -26,17 +27,9 @@ def is_production() -> bool:
 def get_records(
     sf: Salesforce,
     sobject: str,
-    state: t.Optional[dict] = None,
+    last_state: t.Optional[dict] = None,
     replication_key: t.Optional[str] = None,
-    fields: t.Optional[t.List[str]] = None,
 ) -> t.Iterable[dict]:
-    """Return an iterable of records from the Salesforce API."""
-    print(f"Starting {sobject} extraction...")
-    assert not (
-        (state is not None) ^ bool(replication_key)
-    ), f"State and replication key must be used together: {state=}, {replication_key=}"
-    if state:
-        print("Init state:", state)
 
     # Get all fields for the sobject
     desc = getattr(sf, sobject).describe()
@@ -51,22 +44,19 @@ def get_records(
         f["name"] for f in desc["fields"] if f["type"] in ("datetime",) and f["name"]
     }
     # If no fields are specified, use all fields except compound fields
-    if not fields:
-        fields = [f["name"] for f in desc["fields"] if f["name"] not in compound_fields]
+    fields = [f["name"] for f in desc["fields"] if f["name"] not in compound_fields]
 
     # Generate a predicate to filter records by the replication key
     predicate, order_by, n_records = "", "", 0
     if replication_key:
-        bookmark = state.get("last_replication_key")
-        if bookmark:
-            predicate = f"WHERE {replication_key} > {bookmark}"
+        if last_state:
+            predicate = f"WHERE {replication_key} > {last_state}"
         order_by = f"ORDER BY {replication_key} ASC"
     query = f"SELECT {', '.join(fields)} FROM {sobject} {predicate} {order_by}"
     if not is_production():
         query += " LIMIT 100"
 
     # Query all records in batches
-    print(query)
     for page in getattr(sf.bulk, sobject).query_all(query, lazy_operation=True):
         for record in page:
             # Strip out the attributes field
@@ -80,67 +70,6 @@ def get_records(
         yield page
         n_records += len(page)
 
-        # Update the state with the last replication key
-        if replication_key and page:
-            # Assume the records are sorted by replication key
-            state["last_replication_key"] = max(
-                state.get("last_replication_key", ""),
-                page[-1][replication_key],
-            )
-
-    print(f"Done with {sobject} extraction. {n_records} records extracted.")
-    if state and n_records:
-        print("New state:", state)
-
-
-def sobject_factory(
-    name: str,
-    sobject: str,
-    client: Salesforce,
-    write_disposition: str = "replace",
-    replication_key: t.Optional[str] = None,
-    deferrable: bool = False,
-    fields: t.Optional[t.List[str]] = None,
-) -> t.Callable[[], t.Iterable[dict]]:
-    """Return a resource for a Salesforce sobject.
-
-    Args:
-        name: The name of the resource. This affects the table name in the warehouse.
-        sobject: The Salesforce object API name.
-        client: A Salesforce client.
-        write_disposition: The write disposition for the resource.
-        replication_key: The field to use for incremental replication.
-        deferrable: Whether the resource should be deferrable. We recommend not
-            deferring large resources as it could cause memory issues.
-        fields: A list of fields to extract. If not specified, all fields
-            will be extracted.
-    """
-
-    @dlt.resource(name=name, write_disposition=write_disposition)
-    def sobject_resource():
-        nonlocal write_disposition, replication_key
-
-        if replication_key and write_disposition == "append":
-            state: dict = dlt.state().setdefault(name, {})
-        else:
-            replication_key = state = None
-
-        if deferrable:
-            yield dlt.defer(
-                lambda: [
-                    record
-                    for page in get_records(
-                        client, sobject, state, replication_key, fields
-                    )
-                    for record in page
-                ]
-            )
-        else:
-            yield from get_records(client, sobject, state, replication_key, fields)
-
-    return sobject_resource
-
-
 @dlt.source(name="salesforce")
 def salesforce_source(
     username: str = dlt.secrets.value,
@@ -148,79 +77,66 @@ def salesforce_source(
     security_token: str = dlt.secrets.value,
 ):
     client = Salesforce(username, password, security_token)
-    # You can add more sobjects here, custom ones too
-    # See standard objects here:
-    # https://developer.salesforce.com/docs/atlas.en-us.object_reference.meta/object_reference/sforce_api_objects_list.htm
-    # See the `sobject_factory` function docstring for info on the arguments
-    return (
-        sobject_factory(name="sf_user", sobject="User", client=client, deferrable=True),
-        sobject_factory(
-            name="user_role", sobject="UserRole", client=client, deferrable=True
-        ),
-        sobject_factory(
-            name="opportunity",
-            sobject="Opportunity",
-            client=client,
-            write_disposition="append",
-            replication_key="SystemModstamp",
-        ),
-        sobject_factory(
-            name="opportunity_line_item",
-            sobject="OpportunityLineItem",
-            client=client,
-            write_disposition="append",
-            replication_key="SystemModstamp",
-        ),
-        sobject_factory(
-            name="opportunity_contact_role",
-            sobject="OpportunityContactRole",
-            client=client,
-            write_disposition="append",
-            replication_key="SystemModstamp",
-        ),
-        sobject_factory(
-            name="account",
-            sobject="Account",
-            client=client,
-            write_disposition="append",
-            replication_key="LastModifiedDate",
-        ),
-        sobject_factory(name="contact", sobject="Contact", client=client),
-        sobject_factory(name="lead", sobject="Lead", client=client),
-        sobject_factory(
-            name="campaign", sobject="Campaign", client=client, deferrable=True
-        ),
-        sobject_factory(
-            name="campaign_member",
-            sobject="CampaignMember",
-            client=client,
-            write_disposition="append",
-            replication_key="SystemModstamp",
-        ),
-        sobject_factory(
-            name="product_2", sobject="Product2", client=client, deferrable=True
-        ),
-        sobject_factory(
-            name="pricebook_2", sobject="Pricebook2", client=client, deferrable=True
-        ),
-        sobject_factory(
-            name="pricebook_entry",
-            sobject="PricebookEntry",
-            client=client,
-            deferrable=True,
-        ),
-        sobject_factory(
-            name="task",
-            sobject="Task",
-            client=client,
-            write_disposition="append",
-            replication_key="SystemModstamp",
-        ),
-        sobject_factory(
-            name="event",
-            sobject="Event",
-            client=client,
-            write_disposition="append",
-            replication_key="SystemModstamp",
-        ),
-    )
+
+    # define resources
+    @dlt.resource(write_disposition="replace")
+    def sf_user() -> Iterator[Dict[str, Any]]:
+        yield from get_records(client, "User")
+
+    @dlt.resource(write_disposition="replace")
+    def user_role() -> Iterator[Dict[str, Any]]:
+        yield from get_records(client, "UserRole")
+
+    @dlt.resource(write_disposition="merge")
+    def opportunity(last_timestamp = dlt.sources.incremental("SystemModstamp", initial_value=None)) -> Iterator[Dict[str, Any]]:
+        yield from get_records(client, "Opportunity", last_timestamp.last_value, "SystemModstamp")
+
+    @dlt.resource(write_disposition="merge")
+    def opportunity_line_item(last_timestamp = dlt.sources.incremental("SystemModstamp", initial_value=None)) -> Iterator[Dict[str, Any]]:
+        yield from get_records(client, "OpportunityLineItem", last_timestamp.last_value, "SystemModstamp")
+
+    @dlt.resource(write_disposition="merge")
+    def opportunity_contact_role(last_timestamp = dlt.sources.incremental("SystemModstamp", initial_value=None)) -> Iterator[Dict[str, Any]]:
+        yield from get_records(client, "OpportunityContactRole", last_timestamp.last_value, "SystemModstamp")
+
+    @dlt.resource(write_disposition="merge")
+    def account(last_timestamp = dlt.sources.incremental("LastModifiedDate", initial_value=None)) -> Iterator[Dict[str, Any]]:
+        yield from get_records(client, "Account", last_timestamp.last_value, "LastModifiedDate")
+
+    @dlt.resource(write_disposition="replace")
+    def contact() -> Iterator[Dict[str, Any]]:
+        yield from get_records(client, "Contact")
+
+    @dlt.resource(write_disposition="replace")
+    def lead() -> Iterator[Dict[str, Any]]:
+        yield from get_records(client, "Lead")
+
+    @dlt.resource(write_disposition="replace")
+    def campaign() -> Iterator[Dict[str, Any]]:
+        yield from get_records(client, "Campaign")
+
+    @dlt.resource(write_disposition="merge")
+    def campaign_member(last_timestamp = dlt.sources.incremental("SystemModstamp", initial_value=None)) -> Iterator[Dict[str, Any]]:
+        yield from get_records(client, "CampaignMember", last_timestamp.last_value, "SystemModstamp")
+
+    @dlt.resource(write_disposition="replace")
+    def product_2() -> Iterator[Dict[str, Any]]:
+        yield from get_records(client, "Product2")
+
+    @dlt.resource(write_disposition="replace")
+    def pricebook_2() -> Iterator[Dict[str, Any]]:
+        yield from get_records(client, "Pricebook2")
+
+    @dlt.resource(write_disposition="replace")
+    def pricebook_entry() -> Iterator[Dict[str, Any]]:
+        yield from get_records(client, "PricebookEntry")
+
+    @dlt.resource(write_disposition="merge")
+    def task(last_timestamp = dlt.sources.incremental("SystemModstamp", initial_value=None)) -> Iterator[Dict[str, Any]]:
+        yield from get_records(client, "Task", last_timestamp.last_value, "SystemModstamp")
+
+    @dlt.resource(write_disposition="merge")
+    def event(last_timestamp = dlt.sources.incremental("SystemModstamp", initial_value=None)) -> Iterator[Dict[str, Any]]:
+        yield from get_records(client, "Event", last_timestamp.last_value, "SystemModstamp")
+
+    return (sf_user, user_role, opportunity,opportunity_line_item, opportunity_contact_role, account, contact, lead, campaign, campaign_member, product_2, pricebook_2,pricebook_entry, task, event)
--- tests/salesforce/test_salesforce_pipeline.py
+++ tests/salesforce/test_salesforce_pipeline.py
@@ -18,12 +18,36 @@ def test_all_resources(destination_name: str) -> None:
 
     # Execute the pipeline
     load_info = pipeline.run(source)
-
-    print(load_info)
     assert_load_info(load_info)
 
     table_names = [t["name"] for t in pipeline.default_schema.data_tables()]
-    print(table_names)
+    table_counts = load_table_counts(pipeline, *table_names)
+
+
+    print(set(table_counts.keys()))
+
+    assert set(table_counts.keys()) >= set(expected_tables)
+    assert table_counts['sf_user'] == 7
+    assert table_counts['account'] == 13
+    assert table_counts['campaign'] == 4
+    assert table_counts['contact'] == 20
+    assert table_counts['lead'] == 22
+    assert table_counts['opportunity'] == 31
+    assert table_counts['pricebook_2'] == 2
+    assert table_counts['pricebook_entry'] == 34
+    assert table_counts['product_2'] == 17
+    assert table_counts['user_role'] == 18
+
+    # Execute the pipeline
+    source = salesforce_source()
+
+    # Add schema hints as needed...
+    source.schema.merge_hints({"not_null": ["id"]})
+
+    # Execute the pipeline
+    load_info = pipeline.run(source)
+
+    table_names = [t["name"] for t in pipeline.default_schema.data_tables()]
     table_counts = load_table_counts(pipeline, *table_names)
 
     assert set(table_counts.keys()) >= set(expected_tables)
@@ -37,4 +61,3 @@ def test_all_resources(destination_name: str) -> None:
     assert table_counts['product_2'] == 17
     assert table_counts['sf_user'] == 7
     assert table_counts['user_role'] == 18
-
