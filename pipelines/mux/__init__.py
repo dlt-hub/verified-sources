e59532bf22e3cb808e2a5158e27dd82119861c1b
Marcin Rudolf <rudolfix@rudolfix.org>
2023-05-25 18:26:37 +0200

    adds mux test

--- pipelines/mux/__init__.py
+++ pipelines/mux/__init__.py
@@ -1,45 +1,43 @@
-"""Loads Mux data using https://docs.mux.com/api-reference"""
+"""Loads Mux views data using https://docs.mux.com/api-reference"""
+
 import dlt
-import time
+from dlt.common import pendulum
 from dlt.sources.helpers import requests
 from requests.auth import HTTPBasicAuth
-from datetime import datetime, timedelta
 
 API_BASE_URL = 'https://api.mux.com'
 
 
 @dlt.source
-def mux_source(MUX_API_ACCESS_TOKEN=dlt.secrets.value, MUX_API_SECRET_KEY=dlt.secrets.value):
+def mux_source(mux_api_access_token=dlt.secrets.value, mux_api_secret_key=dlt.secrets.value):
     """Passes all video assets and every video view from yesterday to be loaded"""
-    yield assets_resource(MUX_API_ACCESS_TOKEN, MUX_API_SECRET_KEY)
-    yield views_resource(MUX_API_ACCESS_TOKEN, MUX_API_SECRET_KEY)
+    yield assets_resource(mux_api_access_token, mux_api_secret_key)
+    yield views_resource(mux_api_access_token, mux_api_secret_key)
 
 
 @dlt.resource(write_disposition="merge")
-def assets_resource(MUX_API_ACCESS_TOKEN=dlt.secrets.value, MUX_API_SECRET_KEY=dlt.secrets.value):
+def assets_resource(mux_api_access_token=dlt.secrets.value, mux_api_secret_key=dlt.secrets.value, limit=100):
     """Passes metadata about every asset to be loaded"""
     url = f"{API_BASE_URL}/video/v1/assets"
-    limit = 100 # loads only 100 assets
     params = { "limit": limit }
 
-    response = requests.get(url, params=params, auth=HTTPBasicAuth(MUX_API_ACCESS_TOKEN, MUX_API_SECRET_KEY))
+    response = requests.get(url, params=params, auth=HTTPBasicAuth(mux_api_access_token, mux_api_secret_key))
     response.raise_for_status()
     yield response.json()["data"]
 
 
 @dlt.resource(write_disposition="append")
-def views_resource(MUX_API_ACCESS_TOKEN=dlt.secrets.value, MUX_API_SECRET_KEY=dlt.secrets.value):
+def views_resource(mux_api_access_token=dlt.secrets.value, mux_api_secret_key=dlt.secrets.value, limit = 100):
     """Passes metadata about every video view from yesterday to be loaded"""
     url = f"{API_BASE_URL}/data/v1/video-views"
-    limit = 100
     page = 1
-    today = datetime.utcnow().date()
-    yest_start = int(time.mktime((today - timedelta(1)).timetuple()))
-    yest_end = int(time.mktime(today.timetuple()))
+    today = pendulum.today()
+    yest_start = today.subtract(days=1).int_timestamp
+    yest_end = today.int_timestamp
 
     while True:
         params = { "limit": limit, "page": page, "timeframe[]": [yest_start, yest_end] }
-        response = requests.get(url, params=params, auth=HTTPBasicAuth(MUX_API_ACCESS_TOKEN, MUX_API_SECRET_KEY))
+        response = requests.get(url, params=params, auth=HTTPBasicAuth(mux_api_access_token, mux_api_secret_key))
         response.raise_for_status()
         if response.json()["data"] == []:
             break
