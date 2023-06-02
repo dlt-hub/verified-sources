5d747081d3d013a231db29e714ed5ca08faa09d2
David Scharf <shrps@posteo.net>
2023-06-02 18:06:00 +0200

    [cleanup] Shopify pipeline (#162)
    * format
    
    * types and cleanup

--- pipelines/shopify_dlt/__init__.py
+++ pipelines/shopify_dlt/__init__.py
@@ -1,16 +1,17 @@
-
 """Fetches Shopify Orders and Products."""
 
-from typing import Any, Dict, Iterator, Iterator, Optional
+from typing import Any, Dict, Iterator, Iterator, Optional, Iterable
 
 import shopify
 
 import dlt
 
 from dlt.extract.source import DltResource
+from dlt.common.typing import TDataItem
 
-FIRST_DAY_OF_MILLENNIUM = "2000-01-01"
-DEFAULT_API_VERSION = "2023-01"
+from .settings import DEFAULT_API_VERSION, FIRST_DAY_OF_MILLENNIUM
+from .helpers import iterate_page
+
 
 @dlt.source(name="shopify")
 def shopify_source(
@@ -18,14 +19,18 @@ def shopify_source(
     api_version: str = DEFAULT_API_VERSION,
     shop_url: str = dlt.config.value,
     start_date: Optional[str] = FIRST_DAY_OF_MILLENNIUM,
-) -> Iterator[DltResource]:
+) -> Iterable[DltResource]:
     """
-    The source for the shopify pipeline. Available resources are products, orders and customers.
-    @:param: private_app_password - The app password to the app on your shop
-    @:param: api_version - The api version to use
-    @:param: shop_url - The url of your shop
-    @:param: start_date - The date from which to import items, will import items initially created on or after this date
-    @:return: data_resources_list - list of dlt resources
+    The source for the Shopify pipeline. Available resources are products, orders, and customers.
+
+    Args:
+        private_app_password (str): The app password to the app on your shop.
+        api_version (str): The API version to use.
+        shop_url (str): The URL of your shop.
+        start_date (Optional[str]): The date from which to import items. It will import items initially created on or after this date.
+
+    Returns:
+        Iterable[DltResource]: A list of DltResource objects representing the data resources.
     """
 
     # build client
@@ -34,43 +39,63 @@ def shopify_source(
 
     # define resources
     @dlt.resource(primary_key="id", write_disposition="merge")
-    def products(updated_at = dlt.sources.incremental("updated_at", initial_value=FIRST_DAY_OF_MILLENNIUM)) -> Iterator[Dict[str, Any]]:
+    def products(
+        updated_at: dlt.sources.incremental[str] = dlt.sources.incremental(
+            "updated_at", initial_value=FIRST_DAY_OF_MILLENNIUM
+        )
+    ) -> Iterable[TDataItem]:
         """
-        The resource for products on your shop, supports incremental loading and pagination
-        @:param: updated_at - The saved state of the last updated_at value
-        @:return: generator of products
+        The resource for products on your shop, supports incremental loading and pagination.
+
+        Args:
+            updated_at (dlt.sources.incremental[str]): The saved state of the last 'updated_at' value.
+
+        Returns:
+            Iterable[TDataItem]: A generator of products.
         """
-        page = shopify.Product.find(updated_at_min=updated_at.last_value, created_at_min=start_date)
-        while page:
-            for product in page:
-                yield product.to_dict()
-            page = page.next_page() if page.has_next_page() else None
+        page = shopify.Product.find(
+            updated_at_min=updated_at.last_value, created_at_min=start_date
+        )
+        yield iterate_page(page)
 
     @dlt.resource(primary_key="id", write_disposition="merge")
-    def orders(updated_at = dlt.sources.incremental("updated_at", initial_value=FIRST_DAY_OF_MILLENNIUM)) -> Iterator[Dict[str, Any]]:
+    def orders(
+        updated_at: dlt.sources.incremental[str] = dlt.sources.incremental(
+            "updated_at", initial_value=FIRST_DAY_OF_MILLENNIUM
+        )
+    ) -> Iterable[TDataItem]:
         """
-        The resource for orders on your shop, supports incremental loading and pagination
-        @:param: updated_at - The saved state of the last updated_at value
-        @:return: generator of orders
+        The resource for orders on your shop, supports incremental loading and pagination.
+
+        Args:
+            updated_at (dlt.sources.incremental[str]): The saved state of the last 'updated_at' value.
+
+        Returns:
+            Iterable[TDataItem]: A generator of orders.
         """
-        page = shopify.Order.find(updated_at_min=updated_at.last_value, created_at_min=start_date)
-        while page:
-            for order in page:
-                yield order.to_dict()
-            page = page.next_page() if page.has_next_page() else None
+        page = shopify.Order.find(
+            updated_at_min=updated_at.last_value, created_at_min=start_date
+        )
+        yield iterate_page(page)
 
     @dlt.resource(primary_key="id", write_disposition="merge")
-    def customers(updated_at = dlt.sources.incremental("updated_at", initial_value=FIRST_DAY_OF_MILLENNIUM)) -> Iterator[Dict[str, Any]]:
+    def customers(
+        updated_at: dlt.sources.incremental[str] = dlt.sources.incremental(
+            "updated_at", initial_value=FIRST_DAY_OF_MILLENNIUM
+        )
+    ) -> Iterable[TDataItem]:
         """
-        The resource for customers on your shop, supports incremental loading and pagination
-        @:param: updated_at - The saved state of the last updated_at value
-        @:return: generator of customers
+        The resource for customers on your shop, supports incremental loading and pagination.
+
+        Args:
+            updated_at (dlt.sources.incremental[str]): The saved state of the last 'updated_at' value.
+
+        Returns:
+            Iterable[TDataItem]: A generator of customers.
         """
-        page = shopify.Customer.find(updated_at_min=updated_at.last_value, created_at_min=start_date)
-        while page:
-            for order in page:
-                yield order.to_dict()
-            page = page.next_page() if page.has_next_page() else None
+        page = shopify.Customer.find(
+            updated_at_min=updated_at.last_value, created_at_min=start_date
+        )
+        yield iterate_page(page)
 
-    return (products,orders,customers)
-
+    return (products, orders, customers)
