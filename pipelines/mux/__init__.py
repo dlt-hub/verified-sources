8865957a65bf450b98023cee3714dd9902c32b20
David Scharf <shrps@posteo.net>
2023-06-02 18:22:50 +0200

    [cleanup] mux pipeline (#168)
    * format
    
    * small cleanup and typing
    
    * parameters to args

--- pipelines/mux/__init__.py
+++ pipelines/mux/__init__.py
@@ -1,34 +1,73 @@
 """Loads Mux views data using https://docs.mux.com/api-reference"""
 
+from typing import Iterable
+
 import dlt
 from dlt.common import pendulum
 from dlt.sources.helpers import requests
 from requests.auth import HTTPBasicAuth
+from dlt.common.typing import TDataItem
+from dlt.extract.source import DltResource
 
-API_BASE_URL = 'https://api.mux.com'
+from .settings import API_BASE_URL, DEFAULT_LIMIT
 
 
 @dlt.source
-def mux_source(mux_api_access_token=dlt.secrets.value, mux_api_secret_key=dlt.secrets.value):
-    """Passes all video assets and every video view from yesterday to be loaded"""
-    yield assets_resource(mux_api_access_token, mux_api_secret_key)
-    yield views_resource(mux_api_access_token, mux_api_secret_key)
+def mux_source() -> Iterable[DltResource]:
+    """
+    Source function that passes all video assets and every video view from yesterday to be loaded.
+
+    Yields:
+        DltResource: Video assets and video views to be loaded.
+    """
+    yield assets_resource
+    yield views_resource
 
 
 @dlt.resource(write_disposition="merge")
-def assets_resource(mux_api_access_token=dlt.secrets.value, mux_api_secret_key=dlt.secrets.value, limit=100):
-    """Passes metadata about every asset to be loaded"""
-    url = f"{API_BASE_URL}/video/v1/assets"
-    params = { "limit": limit }
+def assets_resource(
+    mux_api_access_token: str = dlt.secrets.value,
+    mux_api_secret_key: str = dlt.secrets.value,
+    limit: int = DEFAULT_LIMIT,
+) -> Iterable[TDataItem]:
+    """
+    Resource function that yields metadata about every asset to be loaded.
 
-    response = requests.get(url, params=params, auth=HTTPBasicAuth(mux_api_access_token, mux_api_secret_key))
+    Args:
+        mux_api_access_token (str): API access token for Mux.
+        mux_api_secret_key (str): API secret key for Mux.
+        limit (int): Limit on the number of assets to retrieve. Defaults to DEFAULT_LIMIT.
+
+    Yields:
+        TDataItem: Data of each asset.
+    """
+    url = f"{API_BASE_URL}/video/v1/assets"
+    params = {"limit": limit}
+
+    response = requests.get(
+        url, params=params, auth=HTTPBasicAuth(mux_api_access_token, mux_api_secret_key)
+    )
     response.raise_for_status()
     yield response.json()["data"]
 
 
 @dlt.resource(write_disposition="append")
-def views_resource(mux_api_access_token=dlt.secrets.value, mux_api_secret_key=dlt.secrets.value, limit = 100):
-    """Passes metadata about every video view from yesterday to be loaded"""
+def views_resource(
+    mux_api_access_token: str = dlt.secrets.value,
+    mux_api_secret_key: str = dlt.secrets.value,
+    limit: int = DEFAULT_LIMIT,
+) -> Iterable[DltResource]:
+    """
+    Resource function that yields metadata about every video view from yesterday to be loaded.
+
+    Args:
+        mux_api_access_token (str): API access token for Mux.
+        mux_api_secret_key (str): API secret key for Mux.
+        limit (int): Limit on the number of video views to retrieve. Defaults to DEFAULT_LIMIT.
+
+    Yields:
+        DltResource: Data for each video view from yesterday.
+    """
     url = f"{API_BASE_URL}/data/v1/video-views"
     page = 1
     today = pendulum.today()
@@ -36,11 +75,14 @@ def views_resource(mux_api_access_token=dlt.secrets.value, mux_api_secret_key=dl
     yest_end = today.int_timestamp
 
     while True:
-        params = { "limit": limit, "page": page, "timeframe[]": [yest_start, yest_end] }
-        response = requests.get(url, params=params, auth=HTTPBasicAuth(mux_api_access_token, mux_api_secret_key))
+        params = {"limit": limit, "page": page, "timeframe[]": [yest_start, yest_end]}
+        response = requests.get(
+            url,
+            params=params,  # type: ignore
+            auth=HTTPBasicAuth(mux_api_access_token, mux_api_secret_key),
+        )
         response.raise_for_status()
         if response.json()["data"] == []:
             break
-        else:
-            yield response.json()["data"]
-            page += 1
+        yield response.json()["data"]
+        page += 1
--- pipelines/mux_pipeline.py
+++ pipelines/mux_pipeline.py
@@ -3,13 +3,12 @@ from mux import mux_source
 
 
 def load_yesterday_video_views():
-
-    pipeline = dlt.pipeline(pipeline_name='mux', destination='duckdb', dataset_name='mux_data')
-
+    pipeline = dlt.pipeline(
+        pipeline_name="mux", destination="duckdb", dataset_name="mux_data"
+    )
     load_info = pipeline.run(mux_source())
-
     print(load_info)
 
 
-if __name__=='__main__':
+if __name__ == "__main__":
     load_yesterday_video_views()
