73f7730fb0bf7df4098a990409015cd5257d29fc
Alena Astrakhantseva <astra92293@gmail.com>
2023-06-08 16:28:36 +0200

    [refactor] reformated docstrings, code formatting

--- sources/jira/__init__.py
+++ sources/jira/__init__.py
@@ -6,6 +6,7 @@ import dlt
 from dlt.common.typing import DictStrAny, TDataItem
 from dlt.extract.source import DltResource
 from dlt.sources.helpers import requests
+
 from .settings import DEFAULT_ENDPOINTS
 
 
@@ -93,7 +94,7 @@ def get_paginated_data(
         api_path: The API path for the Jira endpoint.
         data_path: Optional data path to extract from the response.
         params: Optional parameters for the API request.
-    Returns:
+    Yields:
         Iterable[TDataItem]: Yields pages of data from the API.
     """
     url = f"https://{subdomain}.atlassian.net/{api_path}"
@@ -101,6 +102,7 @@ def get_paginated_data(
     auth = (email, api_token)
     start_at, max_results = 0, 50
     params = {} if params is None else params
+
     while True:
         params["startAt"], params["maxResults"] = start_at, max_results
         response = requests.get(url, auth=auth, headers=headers, params=params)
@@ -112,6 +114,7 @@ def get_paginated_data(
             results_page = response.json()
 
         yield results_page
+
         if len(results_page) < max_results:
             break
         start_at += max_results
--- sources/jira_pipeline.py
+++ sources/jira_pipeline.py
@@ -1,18 +1,22 @@
+from typing import List, Optional
+
 import dlt
 from jira import jira, jira_search
-from typing import List, Optional, Dict
 
 
 def load(endpoints: Optional[List[str]] = None) -> None:
     """
-    Load data from specified Jira endpoints into a BigQuery dataset.
+    Load data from specified Jira endpoints into a dataset.
 
-    :param endpoints: A list of Jira endpoints. If not provided, defaults to all resources.
+    Args:
+        endpoints: A list of Jira endpoints. If not provided, defaults to all resources.
     """
     if not endpoints:
         endpoints = jira().resources.keys()
 
-    pipeline = dlt.pipeline(pipeline_name='strapi', destination='bigquery', dataset_name='jira')
+    pipeline = dlt.pipeline(
+        pipeline_name="jira_pipeline", destination="duckdb", dataset_name="jira"
+    )
 
     load_info = pipeline.run(jira().with_resources(*endpoints))
 
@@ -21,24 +25,29 @@ def load(endpoints: Optional[List[str]] = None) -> None:
 
 def load_query_data(queries: List[str]) -> None:
     """
-    Load data from specified Jira queries into a BigQuery dataset.
+    Load issues from specified Jira queries into a dataset.
 
-    :param queries: A list of JQL queries.
+    Args:
+        queries: A list of JQL queries.
     """
-    pipeline = dlt.pipeline(pipeline_name='jira_search', destination='bigquery', dataset_name='jira_search')
+    pipeline = dlt.pipeline(
+        pipeline_name="jira_search_pipeline",
+        destination="duckdb",
+        dataset_name="jira_search",
+    )
 
     load_info = pipeline.run(jira_search().issues(jql_queries=queries))
 
     print(f"Load Information: {load_info}")
 
 
-if __name__=="__main__":
-    # Add your desired endpoints to the list
-    load()
+if __name__ == "__main__":
+    # Add your desired endpoints to the list 'endpoints'
+    load(endpoints=None)
 
     queries = [
-        'created >= -30d order by created DESC',
-        'created >= -30d AND assignee in (619652abc510bc006b40d007) AND project = DEV AND issuetype = Epic AND status = "In Progress" order by created DESC'
+        "created >= -30d order by created DESC",
+        'created >= -30d AND assignee in (619652abc510bc006b40d007) AND project = DEV AND issuetype = Epic AND status = "In Progress" order by created DESC',
     ]
 
-    load_query_data(queries)
+    load_query_data(queries=queries)
