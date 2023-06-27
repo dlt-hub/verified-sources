17d5a4ee6680b195705aaf67a130e397de7d25f3
Blesson Paul <85806358+Blesson-Paul@users.noreply.github.com>
2023-06-27 16:17:23 +0530

    Update settings.py

--- sources/hubspot/settings.py
+++ sources/hubspot/settings.py
@@ -10,6 +10,6 @@ CRM_CONTACTS_ENDPOINT = (
 CRM_COMPANIES_ENDPOINT = "/crm/v3/objects/companies?properties={properties}associations=contacts,deals,products,tickets,quotes&limit=100"
 CRM_DEALS_ENDPOINT = "/crm/v3/objects/deals?properties={properties}&limit=100"
 CRM_PRODUCTS_ENDPOINT = "/crm/v3/objects/products?limit=100"
-CRM_TICKETS_ENDPOINT = "/crm/v3/objects/tickets?properties={properties}limit=100"
+CRM_TICKETS_ENDPOINT = "/crm/v3/objects/tickets?properties={properties}&limit=100"
 CRM_QUOTES_ENDPOINT = "/crm/v3/objects/quotes?limit=100"
 WEB_ANALYTICS_EVENTS_ENDPOINT = "/events/v3/events?objectType={objectType}&objectId={objectId}&occurredAfter={occurredAfter}&occurredBefore={occurredBefore}&sort=-occurredAt"
