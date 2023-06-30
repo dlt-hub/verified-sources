3634a0c507edab66a85214c104983dc62372a9ee
Alena Astrakhantseva <astra92293@gmail.com>
2023-06-30 15:16:48 +0200

    use one source for unstructured

--- sources/unstructured_data/__init__.py
+++ sources/unstructured_data/__init__.py
@@ -6,29 +6,35 @@ from dlt.extract.source import DltResource, DltSource, TDataItem
 from .helpers import process_file_to_structured, filetype_mapper
 
 
-@dlt.source(name="filesystem")
-def filesystem_source(data_dir=dlt.secrets.value, extensions: Sequence = (".txt",)) -> DltResource:
-
-    @dlt.resource(write_disposition="replace")
-    def local_folder():
-        files = (p.resolve() for p in Path(data_dir).glob("**/*") if p.suffix in set(extensions))
-        for file in files:
-            yield file
-
-    return local_folder
+@dlt.resource(write_disposition="replace")
+def local_folder(data_dir=dlt.secrets.value, extensions: Sequence = (".txt",)):
+    files = (p.resolve() for p in Path(data_dir).glob("**/*") if p.suffix in set(extensions))
+    for file in files:
+        yield {"filepath": str(file)}
 
 
-@dlt.source(name="unstructured")
-def unstructured_source(data_resource: DltResource, queries: Dict[str, str], openai_api_key: str = dlt.secrets.value,) -> DltResource:
-    os.environ['OPENAI_API_KEY'] = openai_api_key
+@dlt.source(name="unstructured", max_table_nesting=3)
+def unstructured_source(queries: Dict[str, str], openai_api_key = dlt.secrets.value,) -> Iterable[DltResource]:
+    if openai_api_key:
+        os.environ['OPENAI_API_KEY'] = openai_api_key
 
     @dlt.transformer(
-        data_from=data_resource,
-        name=f"unstructured_data_from_{data_resource.name}"
+        data_from=local_folder,
+        name=f"unstructured_data_from_{local_folder.name}",
+        write_disposition='replace',
     )
-    def unstructured_resource(file_path) -> Iterable[TDataItem]:
-        extension = file_path.suffix
+    def unstructured_resource(item) -> Iterable[TDataItem]:
+        file_path = item["filepath"]
+        extension = Path(file_path).suffix
         loader = filetype_mapper[extension](file_path)
-        yield process_file_to_structured(loader, queries)
+        yield process_file_to_structured_mock(loader, queries)
 
-    return unstructured_resource
+    return local_folder, unstructured_resource
+
+
+def process_file_to_structured_mock(loader, queries):
+    response = {}
+    for k, query in queries.items():
+        response[k] = query
+
+    return response
