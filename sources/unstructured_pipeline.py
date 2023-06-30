8d4b803645bb16e7ddf2ceab486eafe7aac3f4b3
Alena Astrakhantseva <astra92293@gmail.com>
2023-06-30 15:16:48 +0200

    use one source for unstructured

--- sources/unstructured_pipeline.py
+++ sources/unstructured_pipeline.py
@@ -1,14 +1,12 @@
 import dlt
-from unstructured_data import unstructured_source, filesystem_source
+from unstructured_data import unstructured_source
 
 
 if __name__ == "__main__":
     # configure the pipeline with your destination details
     pipeline = dlt.pipeline(
-        pipeline_name='unstructured', destination='duckdb', dataset_name='unstructured_data'
+        pipeline_name='unstructured', destination='duckdb', dataset_name='unstructured_data', full_refresh=True
     )
-
-    data = filesystem_source().with_resources("local_folder")
 
     queries = {
         "recipient_company_name": "Who is the recipient of the invoice? Just return the name",
@@ -17,10 +15,9 @@ if __name__ == "__main__":
         "invoice_number": "What is the invoice number? Just return the number",
         "service_description": "What is the description of the service that this invoice is for? Just return the description",
     }
-    for resource_name, resource in data.selected_resources.items():
-        data_extractor = unstructured_source(resource, queries)
-        # run the pipeline with your parameters
-        load_info = pipeline.run(data_extractor)
+    data_extractor = unstructured_source(queries)
+    # run the pipeline with your parameters
+    load_info = pipeline.run(data_extractor)
 
-        # pretty print the information on data that was loaded
-        print(load_info)
+    # pretty print the information on data that was loaded
+    print(load_info)
