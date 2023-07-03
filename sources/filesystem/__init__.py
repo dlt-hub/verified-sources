72dd6eaf3e0d6344889a1557b3355bf2e47e53e5
Alena Astrakhantseva <astra92293@gmail.com>
2023-07-03 18:29:54 +0200

    added docstrings, comments, updated readme

--- sources/filesystem/__init__.py
+++ sources/filesystem/__init__.py
@@ -4,7 +4,7 @@ from typing import Optional, Sequence
 import dlt
 from dlt.extract.source import TDataItem
 
-from .helpers import build_service, download_pdf_from_google_drive, get_files_uris
+from .helpers import build_service, download_file_from_google_drive, get_files_uris
 
 
 @dlt.resource(write_disposition="replace")
@@ -12,6 +12,19 @@ def local_folder(
     extensions: Optional[Sequence[str]] = None,
     data_dir: str = dlt.secrets.value,
 ) -> TDataItem:
+    """
+    Retrieves a list of files from a local folder.
+
+    Args:
+        extensions (Optional[Sequence[str]]): A list of file extensions to filter the files.
+            Only files with these extensions will be returned. If None, all files will be returned.
+            Defaults to None.
+        data_dir (str): The path to the local folder from which to retrieve the files.
+            Defaults to the value of `dlt.secrets.value`.
+
+    Yields:
+        TDataItem: A dictionary representing a file, containing the file path.
+    """
     if extensions:
         files = (
             p.resolve()
@@ -35,6 +48,34 @@ def google_drive(
     storage_folder_path: str = dlt.secrets.value,
     download: bool = False,
 ) -> TDataItem:
+    """
+    Retrieves files from a specified Google Drive folder.
+
+    Args:
+        extensions (Sequence[str]): A sequence of file extensions to filter the files.
+            Only files with these extensions will be retrieved.
+            Defaults to (".txt", ".pdf").
+        credentials_path (str): The path to the credentials JSON file for authenticating with Google Drive API.
+            Defaults to the value of `dlt.secrets.value`.
+        token_path (str): The path to the token JSON file for storing and reusing the authentication token.
+            Defaults to the value of `dlt.secrets.value`.
+        folder_id (str): The ID of the Google Drive folder from which to retrieve the files.
+            Defaults to the value of `dlt.secrets.value`.
+        storage_folder_path (str): The path to the local folder where the downloaded files will be stored.
+            Defaults to the value of `dlt.secrets.value`.
+        download (bool): Indicates whether to download the files or not.
+            If True, the files will be downloaded and stored locally.
+            If False, only the file names and IDs will be yielded without downloading.
+            Defaults to False.
+
+    Yields:
+        TDataItem: A dictionary representing a file. If download is True, the dictionary contains the following keys:
+                - "file_path" (str): The local path of the downloaded file.
+            If download is False, the dictionary contains the following keys:
+                - "file_name" (str): The name of the file.
+                - "file_id" (str): The ID of the file in Google Drive.
+    """
+    # create drive api client
     service = build_service(credentials_path, token_path)
     uris = get_files_uris(service, folder_id, extensions=extensions)
     storage_folder_path = Path(storage_folder_path)
@@ -46,9 +87,9 @@ def google_drive(
         if download:
             file_path = storage_folder_path / file_name
 
-            download_pdf_from_google_drive(service, file_id, file_path.as_posix())
+            download_file_from_google_drive(service, file_id, file_path.as_posix())
 
             if file_path.is_file():
-                yield {"file_path": file_path.as_posix(), "file_name": file_name}
+                yield {"file_path": file_path.as_posix()}
         else:
             yield {"file_name": file_name, "file_id": file_id}
