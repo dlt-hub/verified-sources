b618ab9393031cff1baeb1373109f341c87b0aa1
Alena Astrakhantseva <astra92293@gmail.com>
2023-07-05 14:41:20 +0200

    fix path to tests data

--- tests/filesystem/conftest.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from pathlib import Path
-
-
-@pytest.fixture
-def data_dir() -> str:
-    current_dir = Path(__file__).parent.resolve()
-    return (current_dir / "test_data").as_posix()
