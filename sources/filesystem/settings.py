138ba714d6c9fe3c7c6a1d1d207b4213dcab537b
Alena Astrakhantseva <astra92293@gmail.com>
2023-07-06 15:37:25 +0200

    refactoring: add docstrings, fixes, move constants to settings

---
+++ sources/filesystem/settings.py
@@ -0,0 +1,11 @@
+# Google Drive
+SCOPES = ["https://www.googleapis.com/auth/drive.readonly"]
+MIME_TYPE_MAPPER = {".txt": "text/plain", ".pdf": "application/pdf"}
+
+ClIENT_SECRET_PATH = "client_secret.json"
+AUTHORIZED_USER_PATH = "token.json"
+STORAGE_FOLDER_PATH = "temp"
+FOLDER_IDS = ["1-yiloGjyl9g40VguIE1QnY5tcRPaF0Nm"]
+
+# Local folder
+DATA_DIR = "test_data"
