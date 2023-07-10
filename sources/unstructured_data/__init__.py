993202f72bb2ccc07af0a50f18fb6b29add347e4
Alena Astrakhantseva <astra92293@gmail.com>
2023-07-10 16:16:11 +0200

    use pipe for transformers

--- sources/unstructured_data/__init__.py
+++ sources/unstructured_data/__init__.py
@@ -3,52 +3,46 @@
 import logging
 import os
 from pathlib import Path
-from typing import Dict, Iterable
+from typing import Dict
 
 import dlt
-from dlt.extract.source import DltResource, TDataItem
+from dlt.extract.source import TDataItem
 
 from .helpers import filetype_mapper, process_file_to_structured
 
 logging.basicConfig(format="%(asctime)s WARNING: %(message)s", level=logging.WARNING)
 
 
-@dlt.source(name="unstructured_to_structured")
-def unstructured_to_structured_source(
-    unstructured_resource: DltResource,
+@dlt.transformer
+def unstructured_to_structured_resource(
+    unstructured_item: TDataItem,
     queries: Dict[str, str],
     openai_api_key: str = dlt.secrets.value,
-) -> Iterable[DltResource]:
+) -> TDataItem:
     """
-    Converts unstructured data from a specified data resource to structured data using provided queries.
+    Converts unstructured data from a specified data item to structured data using provided queries.
 
     Args:
-        unstructured_resource (DltResource): The data resource containing unstructured data to be converted.
+        unstructured_item (TDataItem): The data item containing unstructured data to be converted.
         queries (Dict[str, str]): A dictionary of queries to be applied to the unstructured data during processing.
             Each query maps a field name to a query string that specifies how to process the field.
         openai_api_key (str): The API key for the OpenAI API. If provided, it sets the `OPENAI_API_KEY` environment variable.
             Defaults to the value of `dlt.secrets.value`.
 
     Returns:
-        Iterable[DltResource]: An iterable containing a resource with original unstructured data and resource with structured data.
+        TDataItem: The structured data item resulting from the conversion.
 
     """
     if openai_api_key:
         os.environ["OPENAI_API_KEY"] = openai_api_key
 
-    @dlt.transformer(
-        data_from=unstructured_resource,
-        name=f"structured_data_from_{unstructured_resource.name}",
-    )
-    def structured_resource(unstructured_item: TDataItem) -> Iterable[TDataItem]:
-        file_path = unstructured_item["file_path"]
-        extension = Path(file_path).suffix
-        if extension in filetype_mapper:
-            loader = filetype_mapper[extension](file_path)
-            yield process_file_to_structured(loader, queries)
-        else:
-            logging.warning(
-                f"Extension {extension} is not implemented, only ({', '.join(filetype_mapper.keys())}) are available."
-            )
+    file_path = unstructured_item["file_path"]
+    extension = Path(file_path).suffix
+    if extension in filetype_mapper:
+        loader = filetype_mapper[extension](file_path)
+        yield process_file_to_structured(loader, queries)
+    else:
+        logging.warning(
+            f"Extension {extension} is not implemented, only ({', '.join(filetype_mapper.keys())}) are available."
+        )
 
-    return unstructured_resource, structured_resource  # type: ignore
