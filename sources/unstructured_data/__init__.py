70fe04a1d81ac68f15bea48ed18614dad73bca23
Alena Astrakhantseva <astra92293@gmail.com>
2023-07-10 16:57:48 +0200

    dynamic names for transformer

--- sources/unstructured_data/__init__.py
+++ sources/unstructured_data/__init__.py
@@ -6,37 +6,53 @@ from pathlib import Path
 from typing import Dict
 
 import dlt
-from dlt.extract.source import TDataItem
+from dlt.extract.source import TDataItem, DltResource
 
 from .helpers import filetype_mapper, process_file_to_structured
 
 logging.basicConfig(format="%(asctime)s WARNING: %(message)s", level=logging.WARNING)
 
 
-@dlt.transformer
+@dlt.resource
 def unstructured_to_structured_resource(
-    unstructured_item: TDataItem,
     queries: Dict[str, str],
     openai_api_key: str = dlt.secrets.value,
-) -> TDataItem:
+    table_name: str = "unstructured_to_structured_resource",
+) -> DltResource:
     """
     Converts unstructured data from a specified data item to structured data using provided queries.
 
     Args:
-        unstructured_item (TDataItem): The data item containing unstructured data to be converted.
         queries (Dict[str, str]): A dictionary of queries to be applied to the unstructured data during processing.
             Each query maps a field name to a query string that specifies how to process the field.
         openai_api_key (str): The API key for the OpenAI API. If provided, it sets the `OPENAI_API_KEY` environment variable.
             Defaults to the value of `dlt.secrets.value`.
+        table_name (str): The name of the table associated with the resource. Defaults to "unstructured_to_structured_resource".
 
     Returns:
-        TDataItem: The structured data item resulting from the conversion.
+        DltResource: A resource-transformer object representing the conversion of unstructured data to structured data.
 
     """
     if openai_api_key:
         os.environ["OPENAI_API_KEY"] = openai_api_key
 
-    file_path = unstructured_item["file_path"]
+    return dlt.transformer(convert_data, name=table_name)(queries)
+
+
+def convert_data(item: TDataItem, queries: Dict[str, str]) -> TDataItem:
+    """
+    Converts unstructured data item to structured data item using provided queries.
+
+    Args:
+        item (TDataItem): The data item containing unstructured data to be converted.
+        queries (Dict[str, str]): A dictionary of queries to be applied to the unstructured data during processing.
+            Each query maps a field name to a query string that specifies how to process the field.
+
+    Returns:
+        TDataItem: The structured data item resulting from the conversion.
+
+    """
+    file_path = item["file_path"]
     extension = Path(file_path).suffix
     if extension in filetype_mapper:
         loader = filetype_mapper[extension](file_path)
@@ -45,4 +61,3 @@ def unstructured_to_structured_resource(
         logging.warning(
             f"Extension {extension} is not implemented, only ({', '.join(filetype_mapper.keys())}) are available."
         )
-
