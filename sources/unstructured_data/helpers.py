e4215df326c8afb1ed6f4f797309950b3abcc70b
Alena Astrakhantseva <astra92293@gmail.com>
2023-07-11 16:42:42 +0200

    use one UnstructuredFileLoader for all types of files, add more vector databases

--- sources/unstructured_data/helpers.py
+++ sources/unstructured_data/helpers.py
@@ -1,14 +1,20 @@
 import logging
-from typing import Any, Dict
+from typing import Any, Dict, List, Type, Union
 
-from langchain.document_loaders import UnstructuredFileLoader, UnstructuredPDFLoader
+from langchain.document_loaders import UnstructuredFileLoader
 from langchain.indexes import VectorstoreIndexCreator
+from langchain.vectorstores.base import VectorStore
+from langchain.vectorstores.chroma import Chroma
+from langchain.vectorstores.elastic_vector_search import ElasticVectorSearch
+from langchain.vectorstores.weaviate import Weaviate
 
 logging.basicConfig(format="%(asctime)s WARNING: %(message)s", level=logging.WARNING)
 
-filetype_mapper = {
-    ".txt": UnstructuredFileLoader,
-    ".pdf": UnstructuredPDFLoader,
+
+vectorstore_mapping = {
+    "chroma": Chroma,
+    "elastic_search": ElasticVectorSearch,
+    "weaviate": Weaviate,
 }
 
 
@@ -16,22 +22,28 @@ def safely_query_index(index: Any, query: str) -> Any:
     return index.query(query).strip()
 
 
-def process_file_to_structured(loader: Any, queries: Dict[str, str]) -> Dict[str, Any]:
+def process_file_to_structured(
+    file_path: Union[str, List[str]],
+    queries: Dict[str, str],
+    vectorstore: Type[VectorStore] = Chroma,
+) -> Dict[str, Any]:
     """
     Processes a file loaded by the specified loader and generates structured data based on provided queries.
 
     Args:
-        loader: (Any) The loader that uses unstructured to load files. E.g. UnstructuredFileLoader, UnstructuredPDFLoader.
+        file_path (Union[str, List[str]]): filepath to the file with unstructured data.
         queries (Dict[str, str]): A dictionary of queries to be applied to the loaded file.
             Each query maps a field name to a query string that specifies how to process the field.
+        vectorstore (Type[VectorStore]): Vector database type. Subclass of VectorStore. Default to Chroma.
 
     Returns:
         Dict[str, str]: A dictionary containing the processed structured data from the loaded file.
             The dictionary includes a "file_path" key with the path of the loaded file and
             additional keys corresponding to the queried fields and their processed values.
     """
-    index = VectorstoreIndexCreator().from_loaders([loader])
-    response = {"file_path": loader.file_path}
+    loader = UnstructuredFileLoader(file_path)
+    index = VectorstoreIndexCreator(vectorstore_cls=vectorstore).from_loaders([loader])
+    response = {"file_path": file_path}
     for k, query in queries.items():
         response[k] = safely_query_index(index, query)
     return response
