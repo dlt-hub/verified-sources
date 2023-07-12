e02cffb7d3f2d04490f19b77e33407efb4234b27
AstrakhantsevaAA <astra92293@gmail.com>
2023-07-12 17:22:27 +0200

    add async conversion

--- sources/unstructured_data/helpers.py
+++ sources/unstructured_data/helpers.py
@@ -7,6 +7,7 @@ from langchain.vectorstores.chroma import Chroma
 from langchain.vectorstores.elastic_vector_search import ElasticVectorSearch
 from langchain.vectorstores.weaviate import Weaviate
 
+from .async_index import AVectorstoreIndexCreator
 
 vectorstore_mapping = {
     "chroma": Chroma,
@@ -16,7 +17,42 @@ vectorstore_mapping = {
 
 
 def safely_query_index(index: Any, query: str) -> Any:
-    return index.query(query).strip()
+    answer = index.query(query)
+    return answer.strip()
+
+
+async def asafely_query_index(index: Any, query: str) -> Any:
+    answer = await index.aquery(query)
+    return answer.strip()
+
+
+async def aprocess_file_to_structured(
+    file_path: Union[str, List[str]],
+    queries: Dict[str, str],
+    vectorstore: Type[VectorStore] = Chroma,
+) -> Dict[str, Any]:
+    """
+    Async processes a file loaded by the specified loader and generates structured data based on provided queries.
+
+    Args:
+        file_path (Union[str, List[str]]): filepath to the file with unstructured data.
+        queries (Dict[str, str]): A dictionary of queries to be applied to the loaded file.
+            Each query maps a field name to a query string that specifies how to process the field.
+        vectorstore (Type[VectorStore]): Vector database type. Subclass of VectorStore. Default to Chroma.
+
+    Returns:
+        Dict[str, str]: A dictionary containing the processed structured data from the loaded file.
+            The dictionary includes a "file_path" key with the path of the loaded file and
+            additional keys corresponding to the queried fields and their processed values.
+    """
+    loader = UnstructuredFileLoader(file_path)
+    index = AVectorstoreIndexCreator(vectorstore_cls=vectorstore).from_loaders([loader])
+    response = {}
+
+    for k, query in queries.items():
+        response[k] = await asafely_query_index(index, query)
+
+    return response
 
 
 def process_file_to_structured(
@@ -40,7 +76,9 @@ def process_file_to_structured(
     """
     loader = UnstructuredFileLoader(file_path)
     index = VectorstoreIndexCreator(vectorstore_cls=vectorstore).from_loaders([loader])
-    response = {"file_path": file_path}
+    response = {}
+
     for k, query in queries.items():
         response[k] = safely_query_index(index, query)
+
     return response
