3ae96e1a5419eecb46bb7a63f8f1d1217e8bf61f
Steinthor Palsson <steini90@gmail.com>
2023-07-21 12:04:09 -0400

    Date helper ensure timezone

--- sources/shopify_dlt/date_helper.py
+++ sources/shopify_dlt/date_helper.py
@@ -8,10 +8,22 @@ TAnyDateTime = Union[pendulum.DateTime, pendulum.Date, datetime, date, str]
 
 
 def ensure_pendulum_datetime(value: TAnyDateTime) -> pendulum.DateTime:
-    """Coerce a value to a pendulum.DateTime object."""
+    """Coerce a date/time value to a `pendulum.DateTime` object.
+
+    UTC is assumed if the value is not timezone aware.
+
+    Args:
+        value: The value to coerce. Can be a pendulum.DateTime, pendulum.Date, datetime, date or iso date/time str.
+
+    Returns:
+        A timezone aware pendulum.DateTime object.
+    """
     if isinstance(value, datetime):
         # both py datetime and pendulum datetime are handled here
-        return pendulum.instance(value)
+        ret = pendulum.instance(value)
+        if ret.tz is None:
+            return ret.in_tz("UTC")
+        return ret
     elif isinstance(value, date):
         return pendulum.DateTime(value.year, value.month, value.day)
     elif isinstance(value, str):
