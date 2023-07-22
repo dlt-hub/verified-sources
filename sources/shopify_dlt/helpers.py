28f17ca24cfa7c0bc0127a935670c8c74c26fbe7
Steinthor Palsson <steini90@gmail.com>
2023-07-22 11:49:11 -0400

    Parse Links header with requests, use urljoin to join root URL

--- sources/shopify_dlt/helpers.py
+++ sources/shopify_dlt/helpers.py
@@ -1,4 +1,5 @@
 """Shopify source helpers"""
+from urllib.parse import urljoin
 
 from dlt.sources.helpers import requests
 from dlt.common.typing import TDataItem, TDataItems, Dict
@@ -44,7 +45,8 @@ class ShopifyApi:
         Yields:
             List of data items from the page
         """
-        url = f"{self.shop_url}/admin/api/{self.api_version}/{resource}.json"
+        url = urljoin(self.shop_url, f"/admin/api/{self.api_version}/{resource}.json")
+
         headers = {"X-Shopify-Access-Token": self.private_app_password}
         while url:
             response = requests.get(url, params=params, headers=headers)
@@ -52,28 +54,9 @@ class ShopifyApi:
             json = response.json()
             # Get item list from the page
             yield [self._convert_datetime_fields(item) for item in json[resource]]
-            link = response.headers.get("Link")
-            url = self._get_next_page_url(link)
+            url = response.links.get("next", {}).get("url")
             # Query params are included in subsequent page URLs
             params = None
-
-    def _get_next_page_url(self, link_header: Optional[str]) -> Optional[str]:
-        """Get the next page URL from the shopify link header
-
-        Args:
-            link_header: The link header from the response
-                e.g. <https://my-shop.myshopify.com/admin/api/2021-07/products.json?page_info=eyJkaXJlY3Rpb24iOiJwcm9kdWN0X2lkIn0>; rel="next"
-
-        Returns:
-            The next page URL or None if there is no next page
-        """
-        if link_header:
-            for link in link_header.split(","):
-                parts = link.split(";")
-                if len(parts) == 2 and parts[1].strip() == 'rel="next"':
-                    url = parts[0].strip(" <>")
-                    return url
-        return None
 
     def _convert_datetime_fields(self, item: Dict[str, Any]) -> Dict[str, Any]:
         """Convert timestamp fields in the item to pendulum datetime objects
