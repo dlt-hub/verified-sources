612cfdcdb9a01e4557f012d075120a53d138ac50
Josué Sehnem <josue@sehnem.com>
2023-11-22 16:39:39 -0300

    fix attendances

--- sources/personio/__init__.py
+++ sources/personio/__init__.py
@@ -6,9 +6,9 @@ import dlt
 from dlt.common import pendulum
 from dlt.common.time import ensure_pendulum_datetime
 from dlt.common.typing import TAnyDateTime, TDataItem
-from dlt.extract.source import DltResource
+from dlt.sources import DltResource
 
-from .helpers import PersonioApi
+from .helpers import PersonioAPI
 from .settings import DEFAULT_ITEMS_PER_PAGE, FIRST_DAY_OF_MILLENNIUM
 
 
@@ -29,7 +29,7 @@ def personio_source(
         Iterable: A list of DltResource objects representing the data resources.
     """
 
-    client = PersonioApi(client_id, client_secret)
+    client = PersonioAPI(client_id, client_secret)
 
     @dlt.resource(primary_key="id", write_disposition="merge")
     def employees(
@@ -50,6 +50,23 @@ def personio_source(
         Returns:
             Iterable: A generator of employees.
         """
+
+        def convert_item(item: TDataItem) -> TDataItem:
+            """Converts an employee item."""
+            attributes = item.get("attributes", {})
+            output = {}
+            for value in attributes.values():
+                name = value["universal_id"]
+                if not name:
+                    label: str = value["label"].replace(" ", "_")
+                    name = label.lower()
+
+                if value["type"] == "date" and value["value"]:
+                    output[name] = ensure_pendulum_datetime(value["value"])
+                else:
+                    output[name] = value["value"]
+            return output
+
         if updated_at.last_value:
             last_value = updated_at.last_value.format("YYYY-MM-DDTHH:mm:ss")
         else:
@@ -57,14 +74,16 @@ def personio_source(
 
         params = {"updated_since": last_value}
 
-        yield from client.get_pages(
+        pages = client.get_pages(
             "company/employees", params=params, page_size=items_per_page
         )
+        for page in pages:
+            yield [convert_item(item) for item in page]
 
-    @dlt.resource(primary_key="id", write_disposition="merge")
+    @dlt.resource(primary_key="id", write_disposition="replace")
     def absences(items_per_page: int = items_per_page) -> Iterable[TDataItem]:
         """
-        The resource for absences, supports incremental loading and pagination.
+        The resource for absences, supports pagination.
 
         Args:
             items_per_page: The max number of items to fetch per page. Defaults to 200.
@@ -73,9 +92,10 @@ def personio_source(
             Iterable: A generator of absences.
         """
 
-        yield from client.get_pages(
-            "company/time-off-types", page_size=items_per_page, convert_response=False
-        )
+        pages = client.get_pages("company/time-off-types", page_size=items_per_page)
+
+        for page in pages:
+            yield [item.get("attributes", {}) for item in page]
 
     @dlt.resource(primary_key="id", write_disposition="merge")
     def attendances(
@@ -84,7 +104,7 @@ def personio_source(
         updated_at: dlt.sources.incremental[
             pendulum.DateTime
         ] = dlt.sources.incremental(
-            "last_modified_at", initial_value=None, allow_external_schedulers=True
+            "updated_at", initial_value=None, allow_external_schedulers=True
         ),
         items_per_page: int = items_per_page,
     ) -> Iterable[TDataItem]:
@@ -94,7 +114,7 @@ def personio_source(
         Args:
             start_date: The start date to fetch attendances from.
             end_date: The end date to fetch attendances from. Defaults to now.
-            updated_at: The saved state of the last 'last_modified_at' value.
+            updated_at: The saved state of the last 'updated_at' value.
             items_per_page: The max number of items to fetch per page. Defaults to 200.
 
         Returns:
@@ -113,8 +133,18 @@ def personio_source(
             "updated_from": updated_iso,
         }
 
-        yield from client.get_pages(
+        pages = client.get_pages(
             "company/attendances", params=params, page_size=items_per_page
         )
+
+        def convert_item(item: TDataItem) -> TDataItem:
+            """Converts an attendance item."""
+            output = dict(id=item["id"], **item.get("attributes"))
+            output["date"] = ensure_pendulum_datetime(output["date"]).date()
+            output["updated_at"] = ensure_pendulum_datetime(output["updated_at"])
+            return output
+
+        for page in pages:
+            yield [convert_item(item) for item in page]
 
     return (employees, absences, attendances)
--- tests/personio/test_personio_source.py
+++ tests/personio/test_personio_source.py
@@ -8,28 +8,50 @@ from tests.utils import ALL_DESTINATIONS, assert_load_info, load_table_counts
 @pytest.mark.parametrize("destination_name", ALL_DESTINATIONS)
 def test_all_resources(destination_name: str) -> None:
     pipeline = dlt.pipeline(
-        pipeline_name="personio",
+        pipeline_name="test_pipeline",
         destination=destination_name,
-        dataset_name="shopify_data",
+        dataset_name="test_data",
         full_refresh=True,
     )
     # Set per page limit to ensure we use pagination
-    tables = ["employees", "absences"]
-    load_info = pipeline.run(personio_source(items_per_page=10).with_resources(*tables))
+    load_info = pipeline.run(personio_source(items_per_page=10))
     print(load_info)
     assert_load_info(load_info)
     table_names = [t["name"] for t in pipeline.default_schema.data_tables()]
     table_counts = load_table_counts(pipeline, *table_names)
 
     # for now only check main tables
+    tables = ["employees", "absences", "attendances"]
     assert set(table_counts.keys()) > set(tables)
     assert table_counts["employees"] >= 31
     assert table_counts["absences"] >= 6
+    assert table_counts["attendances"] >= 21
 
     # load again to check there are no dupicates
-    load_info = pipeline.run(personio_source(items_per_page=10).with_resources(*tables))
+    load_info = pipeline.run(personio_source(items_per_page=10))
     table_names = [t["name"] for t in pipeline.default_schema.data_tables()]
     table_counts = load_table_counts(pipeline, *table_names)
     assert set(table_counts.keys()) > set(tables)
     assert table_counts["employees"] >= 31
     assert table_counts["absences"] >= 6
+    assert table_counts["attendances"] >= 21
+
+
+@pytest.mark.parametrize("destination_name", ALL_DESTINATIONS)
+def test_incremental_endpoints(destination_name: str) -> None:
+    # do the initial load
+    pipeline = dlt.pipeline(
+        pipeline_name="test_pipeline",
+        destination=destination_name,
+        dataset_name="test_data",
+        full_refresh=True,
+    )
+    info = pipeline.run(personio_source().with_resources("employees"))
+    assert_load_info(info)
+    # Personio will bring again the last record, so we expect 1 more load package
+    assert_load_info(info, expected_load_packages=1)
+
+    info = pipeline.run(personio_source().with_resources("attendances"))
+    assert_load_info(info)
+    # Personio will bring again the last record, so we expect 1 more load package
+    assert_load_info(info, expected_load_packages=1)
