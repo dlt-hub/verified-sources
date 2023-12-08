edfaf6ca0f823b2d1179eb40de01ed66da01c71b
AstrakhantsevaAA <astra92293@gmail.com>
2023-12-08 16:43:15 +0100

    fix num of attendances

--- tests/personio/test_personio_source.py
+++ tests/personio/test_personio_source.py
@@ -25,9 +25,9 @@ def test_all_resources(destination_name: str) -> None:
     assert set(table_counts.keys()) > set(tables)
     assert table_counts["employees"] >= 31
     assert table_counts["absences"] >= 6
-    assert table_counts["attendances"] >= 21
+    assert table_counts["attendances"] > 0
 
-    # load again to check there are no dupicates
+    # load again to check there are no duplicates
     load_info = pipeline.run(personio_source(items_per_page=10))
     table_names = [t["name"] for t in pipeline.default_schema.data_tables()]
     table_counts = load_table_counts(pipeline, *table_names)
