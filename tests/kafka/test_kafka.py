236f992c3017c331743af19ab9ead89658a8ff88
IlyaFaer <gurovilya9@gmail.com>
2023-12-28 17:05:01 +0400

    add pause after topics creation

--- tests/kafka/test_kafka.py
+++ tests/kafka/test_kafka.py
@@ -64,7 +64,7 @@ def kafka_topics(kafka_admin):
         t_names.append(name)
 
     _await(kafka_admin.create_topics(new_topics))
-    time.sleep(5)
+    time.sleep(10)
 
     try:
         yield t_names
