3010970faca6d4d89ab81a8d55966da1111b0020
Sultan Iman <sultan@dlthub.com>
2024-01-17 11:48:30 +0100

    Fix mypy issues

--- sources/scraping/types.py
+++ sources/scraping/types.py
@@ -4,9 +4,16 @@ from scrapy.http import Response  # type: ignore
 
 
 # Please read more at https://mypy.readthedocs.io/en/stable/runtime_troubles.html#not-generic-runtime
+# Mypy will also complain down the line about generic T being not subscriptable please read
+# https://github.com/python/mypy/issues/5264#issuecomment-399407428
 if TYPE_CHECKING:
     T = TypeVar("T")
     BaseQueue = Queue[T]
+    class _Queue:
+        def __getitem__(*args):
+            return Queue
+
+    BaseQueue = _Queue()
 else:
     BaseQueue = Queue
 
