4812fcc85df901f8c61eea8b3d5cb82ac2e52d4a
Sultan Iman <sultan@dlthub.com>
2024-02-05 13:59:31 +0100

    Refactor scrapy item pipeline

--- sources/scraping/scrapy/pipeline_item.py
+++ sources/scraping/scrapy/pipeline_item.py
@@ -1,18 +1,46 @@
+from abc import ABC
 from typing import Type
+
 import scrapy  # type: ignore
+
+from scrapy.exceptions import CloseSpider
 from dlt.common import logger
 
-from .spider import DltSpider
+from ..queue import BaseQueue
 
 
-class ScrapingPipelineItem:
+class PipelineItem(ABC):
     def process_item(
-        self, item: Type[scrapy.Item], spider: Type[DltSpider]
+        self, item: Type[scrapy.Item], spider: scrapy.Spider
     ) -> Type[scrapy.Item]:
-        """Handles sending the result of spider to scraping resource of Dlt pipeline"""
-        if not spider.queue.is_closed:
-            spider.queue.put(item)  # type: ignore
-        else:
-            logger.warning("Queue is closed")
+        raise NotImplementedError
 
-        return item
+
+def get_pipeline_item(queue: Type[BaseQueue]) -> Type[PipelineItem]:
+    """Wraps our custom ItemPipeline and provides queue instance
+
+    It is done this way because there is no way to define
+    custom initializer for scrapy ItemPipeline similar to closures.
+    https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+    Args:
+        queue (Type[BaseQueue]): queue instance
+
+    Returns:
+        (Type[PipelineItem]): Custom pipeline item with access to queue
+    """
+
+    class ScrapingPipelineItem(PipelineItem):
+        def process_item(
+            self, item: Type[scrapy.Item], spider: scrapy.Spider
+        ) -> Type[scrapy.Item]:
+            """Handles sending the result of spider to scraping resource of Dlt pipeline"""
+            if not queue.is_closed:
+                queue.put(item)  # type: ignore
+            else:
+                logger.error("Queue is closed")
+                raise CloseSpider("Queue is closed")
+
+            return item
+
+    return ScrapingPipelineItem
