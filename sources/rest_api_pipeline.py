76ffbd9139e5880fec46fceb1652ada28f0187d5
Anton Burnashev <anton.burnashev@gmail.com>
2024-02-05 17:26:41 +0100

    Remove the example

--- sources/rest_api_pipeline.py
+++ sources/rest_api_pipeline.py
@@ -2,42 +2,6 @@ import dlt
 from dlt.sources.helpers import requests
 from rest_api import rest_api_source
 
-#
-# dlt Requests:
-#
-
-
-@dlt.resource(
-    table_name="issues",
-    write_disposition="merge",
-    primary_key="id",
-)
-def get_issues(
-    updated_at=dlt.sources.incremental(
-        "updated_at", initial_value="1970-01-01T00:00:00Z"
-    )
-):
-    url = (
-        f"https://api.github.com/repos/dlt-hub/dlt/issues"
-        f"?since={updated_at.last_value}&per_page=100"
-        f"&sort=updated&directions=desc&state=open"
-    )
-
-    while True:
-        response = requests.get(url)
-        response.raise_for_status()
-        yield response.json()
-
-        # Get next page
-        if "next" not in response.links:
-            break
-        url = response.links["next"]["url"]
-
-
-#
-# REST Source:
-#
-
 
 def load_github():
     pipeline = dlt.pipeline(
