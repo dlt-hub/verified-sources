884120b8f0e8931d61dbaef4612767535fcd6389
Anton Burnashev <anton.burnashev@gmail.com>
2024-02-22 01:11:42 +0300

    Remove the legacy version

--- sources/rest_api_pipeline.py
+++ sources/rest_api_pipeline.py
@@ -1,11 +1,5 @@
 import dlt
-from rest_api import (
-    rest_api_source,
-    ResolveConfig as resolve_from,
-    rest_api_resources_v2,
-)
-from rest_api import RESTClient, EndpointResource, Endpoint
-from rest_api.auth import BearerTokenAuth
+from rest_api import rest_api_source
 
 
 def load_github():
@@ -71,98 +65,6 @@ def load_github():
                 },
             ],
         }
-    )
-
-    load_info = pipeline.run(github_source)
-    print(load_info)
-
-
-def load_github_legacy():
-    pipeline = dlt.pipeline(
-        pipeline_name="rest_api_github_v1",
-        destination="duckdb",
-        dataset_name="rest_api_data",
-    )
-
-    github_source = rest_api_source(
-        {
-            "client": {
-                "base_url": "https://api.github.com/repos/dlt-hub/dlt/",
-                # If you leave out the paginator, it will be inferred from the API:
-                # "paginator": "header_links",
-                "auth": {
-                    "token": dlt.secrets["github_token"],
-                },
-            },
-            "endpoints": {
-                "issues/{issue_number}/comments": {
-                    "params": {
-                        "per_page": 100,
-                        "issue_number": resolve_from("issues", "number"),
-                    },
-                    "resource": {
-                        "primary_key": "id",
-                    },
-                },
-                "issues": {
-                    "params": {
-                        "per_page": 100,
-                        "sort": "updated",
-                        "direction": "desc",
-                        "state": "open",
-                        "since": dlt.sources.incremental(
-                            "updated_at", initial_value="2024-01-25T11:21:28Z"
-                        ),
-                    },
-                    "resource": {
-                        "primary_key": "id",
-                        "write_disposition": "merge",
-                    },
-                },
-            },
-        }
-    )
-
-    load_info = pipeline.run(github_source)
-    print(load_info)
-
-
-def load_github_v2():
-    pipeline = dlt.pipeline(
-        pipeline_name="rest_api_github_v2",
-        destination="duckdb",
-        dataset_name="rest_api_data",
-    )
-
-    github_source = rest_api_resources_v2(
-        RESTClient(
-            base_url="https://api.github.com/repos/dlt-hub/dlt/",
-            auth=BearerTokenAuth(dlt.secrets["github_token"]),
-        ),
-        EndpointResource(
-            Endpoint(
-                "issues",
-                params={
-                    "per_page": 100,
-                    "sort": "updated",
-                    "direction": "desc",
-                    "state": "open",
-                },
-            ),
-            name="issues",
-            primary_key="id",
-            write_disposition="merge",
-        ),
-        EndpointResource(
-            Endpoint(
-                "issues/{issue_number}/comments",
-                params={
-                    "per_page": 100,
-                    "issue_number": resolve_from("issues", "number"),
-                },
-            ),
-            primary_key="id",
-        ),
     )
 
     load_info = pipeline.run(github_source)
