729632401c107c3608aceae73d2ccb2205ea8207
Sultan Iman <sultan.imanhodjaev@gmail.com>
2024-02-22 13:47:27 +0100

    Add more types

--- sources/scraping/types.py
+++ sources/scraping/types.py
@@ -1,4 +1,12 @@
-from typing import Any, Dict
+import typing as t
+
+AnyDict = t.Dict[str, t.Any]
+
+# start_urls can be a file path or a list of urls
+StartUrls = t.Optional[t.Union[t.List[str], str]]
+P = t.ParamSpec("P")
 
 
-AnyDict = Dict[str, Any]
+class Runnable(t.Protocol):
+    def run(self, *args: P.args, **kwargs: P.kwargs) -> t.Any:
+        pass
