83ec743bc50d8e128f76e8b5a9e453c24f6aa91c
Sultan Iman <sultan.imanhodjaev@gmail.com>
2024-02-22 13:50:24 +0100

    Simplify helpers code

--- sources/scraping/helpers.py
+++ sources/scraping/helpers.py
@@ -1,9 +1,6 @@
-import threading
 import typing as t
 
 import dlt
-import scrapy  # type: ignore
-
 from dlt.common import logger
 from dlt.common.configuration.inject import with_config
 from dlt.common.configuration.specs.base_configuration import (
@@ -11,22 +8,14 @@ from dlt.common.configuration.specs.base_configuration import (
     BaseConfiguration,
 )
 
-from scrapy import signals, Item  # type: ignore
+
+from scrapy import Item, Spider  # type: ignore
 from scrapy.exceptions import CloseSpider  # type: ignore
-from scrapy.crawler import CrawlerProcess  # type: ignore
 
-
-from .types import AnyDict
-from .queue import BaseQueue
-from .settings import SOURCE_SCRAPY_SETTINGS, SOURCE_SCRAPY_QUEUE_SIZE
-
-
-T = t.TypeVar("T")
-
-
-class Runnable(t.Protocol):
-    def run(self, *args: t.Any, **kwargs: AnyDict) -> Any:
-        pass
+from .settings import SOURCE_SCRAPY_QUEUE_SIZE, SOURCE_SCRAPY_SETTINGS
+from .queue import ScrapingQueue
+from .runner import ScrapingHost, PipelineRunner, ScrapyRunner
+from .types import StartUrls, P
 
 
 @configspec
@@ -41,55 +30,64 @@ class ScrapingConfig(BaseConfiguration):
     queue_result_timeout: t.Optional[int] = 5
 
     # List of start urls
-    start_urls: t.Optional[t.List[str]] = None
+    start_urls: StartUrls = None
 
 
-class ScrapyRunner(Runnable):
-    def __init__(
-        self,
-        queue: BaseQueue[T],
-        spider: t.Type[scrapy.Spider],
-        start_urls: t.List[str],
-        settings: AnyDict,
-        on_item_scraped: t.Callable[[Item], None],
-        on_engine_stopped: t.Callable[[], None],
-    ) -> None:
-        self.queue = queue
-        self.spider = spider
-        self.settings = settings
-        self.start_urls = start_urls
-        self.on_item_scraped = on_item_scraped
-        self.on_engien_stopped = on_engine_stopped
+def resolve_start_urls(path: StartUrls) -> t.List[str]:
+    if isinstance(path, str):
+        with open(path) as fp:
+            return fp.readlines()
 
-    def run(self, *args: t.Any, **kwargs: AnyDict) -> None:
-        crawler = CrawlerProcess(settings=self.settings)
-        crawler.crawl(
-            self.spider,
-            queue=self.queue,
-            name="scraping_spider",
-            start_urls=self.start_urls,
-            **kwargs,
-        )
-        crawler.signals.connect(self.on_item_scraped, signals.item_scraped)
-        crawler.signals.connect(self.on_engien_stopped, signals.engine_stopped)
-        crawler.start()
+    return path or []
 
-class PipelineRunner(Runnable):
-    def __init__(
-        self,
-        pipeline: dlt.Pipeline,
-        queue: BaseQueue[T],
-    ) -> None:
-        self.pipeline = pipeline
-        self.queue = queue
 
-    def run(  # type: ignore[override]
-        self,
-        data: t.Any,
-        *args: t.Any,
-        **kwargs: t.Any,
-    ) -> None:
-        """You can use all regular dlt.pipeline.run() arguments
+@with_config(sections=("sources", "scraping"), spec=ScrapingConfig)
+def create_pipeline_runner(
+    pipeline: dlt.Pipeline,
+    spider: t.Type[Spider],
+    queue_size: int = dlt.config.value,
+    queue_result_timeout: int = dlt.config.value,
+    start_urls: StartUrls = dlt.config.value,
+) -> ScrapingHost:
+    queue = ScrapingQueue(
+        maxsize=queue_size,
+        read_timeout=queue_result_timeout,
+    )
+
+    def on_item_scraped(item: Item) -> None:
+        if not queue.is_closed:
+            queue.put(item)  # type: ignore
+        else:
+            logger.error("Queue is closed")
+            raise CloseSpider("Queue is closed")
+
+    def on_engine_stopped() -> None:
+        queue.join()
+        queue.close()
+
+    scrapy_runner = ScrapyRunner(
+        queue=queue,
+        spider=spider,
+        start_urls=resolve_start_urls(start_urls),
+        settings=SOURCE_SCRAPY_SETTINGS,
+        on_item_scraped=on_item_scraped,
+        on_engine_stopped=on_engine_stopped,
+    )
+
+    pipeline_runner = PipelineRunner(pipeline=pipeline)
+    scraping_host = ScrapingHost(scrapy_runner, pipeline_runner)
+    return scraping_host
+
+
+def run_pipeline(
+    pipeline: dlt.Pipeline,
+    spider: t.Type[Spider],
+    *args: P.args,
+    **kwargs: P.kwargs,
+) -> None:
+    """Simple runner for the scraping pipeline
+
+    You can pass all parameters via kwargs to `dlt.pipeline.run(....)`
 
         ```
         destination: TDestinationReferenceArg = None,
@@ -103,66 +101,6 @@ class PipelineRunner(Runnable):
         schema: Schema = None,
         loader_file_format: TLoaderFileFormat = None
         ```
-        """
-
-        def run() -> None:
-            try:
-                self.pipeline.run(data, **kwargs)
-            except Exception:
-                logger.error("Error during pipeline.run call, closing the queue")
-                self.queue.close()
-                raise
-
-        self.thread_runner = threading.Thread(target=run)
-        self.thread_runner.start()
-
-    def join(self) -> None:
-        self.thread_runner.join()
-
-
-@with_config(sections=("sources", "scraping"), spec=ScrapingConfig)
-def create_pipeline_runner(
-    pipeline: dlt.Pipeline,
-    queue_size: int = dlt.config.value,
-    spider: t.Type[scrapy.Spider] = None,
-    start_urls: t.List[str] = dlt.config.value,
-) -> t.Tuple[PipelineRunner, ScrapyRunner, t.Callable[[], None]]:
-    queue = BaseQueue(maxsize=queue_size)
-
-    settings = {**SOURCE_SCRAPY_SETTINGS}
-
-    def on_item_scraped(item: Item) -> None:
-        if not queue.is_closed:
-            queue.put(item)  # type: ignore
-        else:
-            logger.error("Queue is closed")
-            raise CloseSpider("Queue is closed")
-
-    def on_engine_stopped() -> None:
-        queue.join()
-        if not queue.is_closed:
-            queue.close()
-
-    scrapy_runner = ScrapyRunner(
-        queue=queue,
-        spider=spider,
-        start_urls=start_urls,
-        settings=settings,
-        on_item_scraped=on_item_scraped,
-        on_engine_stopped=on_engine_stopped,
-    )
-
-    pipeline_runner = PipelineRunner(pipeline=pipeline)
-
-    def wait_for_results() -> None:
-        pipeline_runner.join()
-
-    return pipeline_runner, scrapy_runner, wait_for_results
-
-
-def run_pipeline(
-    pipeline: dlt.Pipeline,
-    spider: t.Type[scrapy.Spider],
-    urls: t.List[str],
-) -> None:
-    create_pipeline_runner(pipeline=pipeline, spider=spider, )
+    """
+    scraping_host = create_pipeline_runner(pipeline, spider)
+    scraping_host.run(*args, **kwargs)
