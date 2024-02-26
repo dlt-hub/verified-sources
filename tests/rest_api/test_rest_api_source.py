ed927ccebaa93ee271b44d376d66b89e09a71d65
Willi <willi.mueller@posteo.de>
2024-02-26 14:55:34 +0530

    makes test case green and fast

--- tests/rest_api/test_rest_api_source.py
+++ tests/rest_api/test_rest_api_source.py
@@ -1,7 +1,7 @@
 import dlt
 import pytest
 
-from sources.rest_api import rest_api_source
+from sources.rest_api import SinglePagePaginator, rest_api_source
 from tests.utils import ALL_DESTINATIONS, assert_load_info, load_table_counts
 
 
@@ -75,8 +75,9 @@ def test_dependent_resource(destination_name: str) -> None:
                 "name": "pokemon_list",
                 "endpoint": {
                     "path": "pokemon",
+                    "paginator": SinglePagePaginator(records_key="results"),
                     "params": {
-                        "limit": "2",
+                        "limit": 2,
                     },
                 },
             },
@@ -91,19 +92,32 @@ def test_dependent_resource(destination_name: str) -> None:
                             "field": "name",
                         },
                     },
+                    "paginator": "single_page",
                 },
             },
         ],
     }
 
-    data = rest_api_source(config)
+    data = (
+        rest_api_source(config).with_resources("pokemon_list", "pokemon")
+    )
     pipeline = _make_pipeline(destination_name)
     load_info = pipeline.run(data)
     assert_load_info(load_info)
     table_names = [t["name"] for t in pipeline.default_schema.data_tables()]
     table_counts = load_table_counts(pipeline, *table_names)
 
-    assert table_counts.keys() == {"pokemon_list", "pokemon"}
+    assert list(table_counts.keys()) == [
+        "pokemon",
+        "pokemon__types",
+        "pokemon__stats",
+        "pokemon__moves__version_group_details",
+        "pokemon__moves",
+        "pokemon__game_indices",
+        "pokemon__forms",
+        "pokemon__abilities",
+        "pokemon_list",
+    ]
 
     assert table_counts["pokemon_list"] == 2
     assert table_counts["pokemon"] == 2
