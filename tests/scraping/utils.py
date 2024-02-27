36e55c55281951bcb6633b3ab8dcda45addbaa02
Sultan Iman <sultan.imanhodjaev@gmail.com>
2024-02-27 17:10:42 +0100

    Adjust typing for scraping testing utils

--- tests/scraping/utils.py
+++ tests/scraping/utils.py
@@ -1,4 +1,4 @@
-from typing import Any, Type
+from typing import Any, Type, Union
 import time
 import threading
 
@@ -38,7 +38,7 @@ class MySpider(Spider):
 class TestCrawlerProcess(CrawlerRunner):
     def crawl(
         self,
-        crawler_or_spidercls: Type[Spider] | str | Crawler,
+        crawler_or_spidercls: Union[Type[Spider] | str | Crawler],
         *args: Any,
         **kwargs: Any,
     ) -> Deferred:
