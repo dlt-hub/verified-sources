7063250511778f82014faca7df8b4570b0a27923
rudolfix <rudolfix@rudolfix.org>
2024-04-11 00:21:54 +0200

    adds defaults to fields in specs (#421)

--- sources/zendesk/helpers/credentials.py
+++ sources/zendesk/helpers/credentials.py
@@ -1,6 +1,7 @@
 """
 This module handles how credentials are read in dlt sources
 """
+import dlt
 from typing import ClassVar, List, Union
 from dlt.common.configuration import configspec
 from dlt.common.configuration.specs import CredentialsConfiguration
@@ -13,7 +14,7 @@ class ZendeskCredentialsBase(CredentialsConfiguration):
     The Base version of all the ZendeskCredential classes.
     """
 
-    subdomain: str
+    subdomain: str = dlt.config.value
     __config_gen_annotations__: ClassVar[List[str]] = []
 
 
@@ -23,8 +24,8 @@ class ZendeskCredentialsEmailPass(ZendeskCredentialsBase):
     This class is used to store credentials for Email + Password Authentication
     """
 
-    email: str
-    password: TSecretValue
+    email: str = dlt.config.value
+    password: TSecretValue = dlt.secrets.value
 
 
 @configspec
@@ -33,7 +34,7 @@ class ZendeskCredentialsOAuth(ZendeskCredentialsBase):
     This class is used to store credentials for OAuth Token Authentication
     """
 
-    oauth_token: TSecretValue
+    oauth_token: TSecretValue = dlt.secrets.value
 
 
 @configspec
@@ -42,8 +43,8 @@ class ZendeskCredentialsToken(ZendeskCredentialsBase):
     This class is used to store credentials for Token Authentication
     """
 
-    email: str
-    token: TSecretValue
+    email: str = dlt.config.value
+    token: TSecretValue = dlt.secrets.value
 
 
 TZendeskCredentials = Union[
