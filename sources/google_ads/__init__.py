bc6ccf9c0f130e1e38f04119456b5b8ebeb99c9e
IlyaFaer <gurovilya9@gmail.com>
2024-04-22 10:20:49 +0400

    lint fix

--- sources/google_ads/__init__.py
+++ sources/google_ads/__init__.py
@@ -2,71 +2,99 @@
 Preliminary implementation of Google Ads pipeline.
 """
 
-from typing import List, Union
+from typing import Any, Dict, Iterator, List, Union
 import dlt
 import tempfile
 from dlt.common.exceptions import MissingDependencyException
+from dlt.common.typing import TDataItem
+from dlt.sources import DltResource
 from dlt.sources.credentials import GcpOAuthCredentials, GcpServiceAccountCredentials
 import json
-from pipelines.google_ads.helpers.data_processing import to_dict
+from pipelines.google_ads.helpers.data_processing import to_dict  # type: ignore
+
+from apiclient.discovery import Resource
+
 try:
-    from google.ads.googleads.client import GoogleAdsClient
+    from google.ads.googleads.client import GoogleAdsClient  # type: ignore
 except ImportError:
     raise MissingDependencyException("Requests-OAuthlib", ["google-ads"])
-try:
-    from google.oauth2.credentials import Credentials
-except ImportError:
-    raise MissingDependencyException("Google OAuth Library", ["google-auth-oauthlib"])
 
 
-DIMENSION_TABLES = ["accounts", "ad_group", "ad_group_ad", "ad_group_ad_label", "ad_group_label", "campaign_label", "click_view", "customer", "keyword_view", "geographic_view"]
+DIMENSION_TABLES = [
+    "accounts",
+    "ad_group",
+    "ad_group_ad",
+    "ad_group_ad_label",
+    "ad_group_label",
+    "campaign_label",
+    "click_view",
+    "customer",
+    "keyword_view",
+    "geographic_view",
+]
 
 
-def get_client(credentials: Union[GcpOAuthCredentials, GcpServiceAccountCredentials], dev_token: str,impersonated_email:str) -> GoogleAdsClient:
+def get_client(
+    credentials: Union[GcpOAuthCredentials, GcpServiceAccountCredentials],
+    dev_token: str,
+    impersonated_email: str,
+) -> GoogleAdsClient:
     # generate access token for credentials if we are using OAuth2.0
     if isinstance(credentials, GcpOAuthCredentials):
         credentials.auth("https://www.googleapis.com/auth/adwords")
-        credentials = {
+        conf = {
             "developer_token": dev_token,
             "use_proto_plus": True,
-            **json.loads(credentials.to_native_representation())
+            **json.loads(credentials.to_native_representation()),
         }
-        return GoogleAdsClient.load_from_dict(config_dict=credentials)
+        return GoogleAdsClient.load_from_dict(config_dict=conf)
     # use service account to authenticate if not using OAuth2.0
     else:
         # google ads client requires the key to be in a file on the disc..
         with tempfile.NamedTemporaryFile() as f:
             f.write(credentials.to_native_representation().encode())
             f.seek(0)
-            return GoogleAdsClient.load_from_dict(config_dict={
-                "json_key_file_path": f.name,
-                "impersonated_email": impersonated_email,
-                "use_proto_plus": True,
-                "developer_token": dev_token,
-            })
+            return GoogleAdsClient.load_from_dict(
+                config_dict={
+                    "json_key_file_path": f.name,
+                    "impersonated_email": impersonated_email,
+                    "use_proto_plus": True,
+                    "developer_token": dev_token,
+                }
+            )
 
 
 @dlt.source(max_table_nesting=2)
-def google_ads(credentials: Union[GcpOAuthCredentials, GcpServiceAccountCredentials] = dlt.secrets.value,
-                impersonated_email: str = dlt.secrets.value,
-               dev_token: str = dlt.secrets.value):
+def google_ads(
+    credentials: Union[
+        GcpOAuthCredentials, GcpServiceAccountCredentials
+    ] = dlt.secrets.value,
+    impersonated_email: str = dlt.secrets.value,
+    dev_token: str = dlt.secrets.value,
+) -> List[DltResource]:
     """
     Loads default tables for google ads in the database.
     :param credentials:
     :param dev_token:
     :return:
     """
-    client = get_client(credentials=credentials, dev_token=dev_token, impersonated_email=impersonated_email)
+    client = get_client(
+        credentials=credentials,
+        dev_token=dev_token,
+        impersonated_email=impersonated_email,
+    )
     return [
         customers(client=client),
         campaigns(client=client),
         change_events(client=client),
-        customer_clients(client=client)
-        ]
+        customer_clients(client=client),
+    ]
 
 
 @dlt.resource(write_disposition="replace")
-def customers(client, customer_id: str = dlt.secrets.value):
+def customers(
+    client: Resource, customer_id: str = dlt.secrets.value
+) -> Iterator[TDataItem]:
     """
     Dlt resource which loads dimensions.
     :param client:
@@ -80,8 +108,11 @@ def customers(client, customer_id: str = dlt.secrets.value):
         for row in batch.results:
             yield to_dict(row.customer)
 
+
 @dlt.resource(write_disposition="replace")
-def campaigns(client, customer_id: str = dlt.secrets.value):
+def campaigns(
+    client: Resource, customer_id: str = dlt.secrets.value
+) -> Iterator[TDataItem]:
     """
     Dlt resource which loads dimensions.
     :param client:
@@ -95,8 +126,11 @@ def campaigns(client, customer_id: str = dlt.secrets.value):
         for row in batch.results:
             yield to_dict(row.campaign)
 
+
 @dlt.resource(write_disposition="replace")
-def change_events(client, customer_id: str = dlt.secrets.value):
+def change_events(
+    client: Resource, customer_id: str = dlt.secrets.value
+) -> Iterator[TDataItem]:
     """
     Dlt resource which loads dimensions.
     :param client:
@@ -110,8 +144,11 @@ def change_events(client, customer_id: str = dlt.secrets.value):
         for row in batch.results:
             yield to_dict(row.change_event)
 
+
 @dlt.resource(write_disposition="replace")
-def customer_clients(client, customer_id: str = dlt.secrets.value):
+def customer_clients(
+    client: Resource, customer_id: str = dlt.secrets.value
+) -> Iterator[TDataItem]:
     """
     Dlt resource which loads dimensions.
     :param client:
--- sources/google_ads_pipeline.py
+++ sources/google_ads_pipeline.py
@@ -1,15 +1,19 @@
-import time
 import dlt
+from dlt.common.pipeline import LoadInfo
 from google_ads import google_ads
 
 
-
-def load_pipeline():
+def load_pipeline() -> LoadInfo:
     """
     Loads custom queries and default tables
     """
 
-    pipeline = dlt.pipeline(pipeline_name="dlt_google_ads_pipeline", destination="duckdb", full_refresh=False, dataset_name="full_load_google_ads")
+    pipeline = dlt.pipeline(
+        pipeline_name="dlt_google_ads_pipeline",
+        destination="duckdb",
+        full_refresh=False,
+        dataset_name="full_load_google_ads",
+    )
     data_default = google_ads()
     info = pipeline.run(data=[data_default])
     return info
@@ -18,4 +22,3 @@ def load_pipeline():
 if __name__ == "__main__":
     load_info = load_pipeline()
     print(load_info)
-
