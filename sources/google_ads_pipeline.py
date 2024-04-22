8cce1b23d6ba327c85d7a37636dfefdc79fd3450
dat-a-man <98139823+dat-a-man@users.noreply.github.com>
2024-04-22 18:58:06 +0530

    Source/add google ads (#428)
    * Testing Google Ads API
    
    * Stubs for sources, resources and tests. General structure for the project
    
    * Filled up resoures and processing with some code.
    
    * Updated dimensions code
    
    * Add helper for retrieving attributes
    
    * first basic working version of google ads pipeline with tests
    
    * fix auth scope
    
    * Commented out Google Ads test
    
    * Added README.md for Google ads.
    Please note that some links in this readme, might not work until the google ads documentation is merged.
    
    * lock dependencies
    
    * lint fix
    
    * change commenting type
    
    * delete excess ignore
    
    ---------
    
    Co-authored-by: teomeskuti <teomeskuti29@gmail.com>
    Co-authored-by: redicane <caneredi@gmail.com>
    Co-authored-by: Dave <shrps@posteo.net>
    Co-authored-by: IlyaFaer <gurovilya9@gmail.com>

---
+++ sources/google_ads_pipeline.py
@@ -0,0 +1,24 @@
+import dlt
+from dlt.common.pipeline import LoadInfo
+from google_ads import google_ads
+
+
+def load_pipeline() -> LoadInfo:
+    """
+    Loads custom queries and default tables
+    """
+
+    pipeline = dlt.pipeline(
+        pipeline_name="dlt_google_ads_pipeline",
+        destination="duckdb",
+        full_refresh=False,
+        dataset_name="full_load_google_ads",
+    )
+    data_default = google_ads()
+    info = pipeline.run(data=[data_default])
+    return info
+
+
+if __name__ == "__main__":
+    load_info = load_pipeline()
+    print(load_info)
