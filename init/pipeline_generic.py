2b8cd3c329aee04b50967ad652da0364acbbd019
Sultan Iman <sultan.imanhodjaev@gmail.com>
2024-05-08 11:44:43 +0200

    Use github rest api

--- init/pipeline_generic.py
+++ init/pipeline_generic.py
@@ -1,71 +1,87 @@
+from typing import Any, Dict, Optional
 import dlt
 
 from dlt.sources.helpers.rest_client.auth import BearerTokenAuth
 from dlt.sources.helpers.rest_client.client import RESTClient
-from dlt.sources.helpers.rest_client.paginators import JSONResponsePaginator
+from dlt.sources.helpers.rest_client.paginators import HeaderLinkPaginator
 
+
+# Note: Ensure the API key (Bearer token) is set up correctly in secrets or environment variables.
 
 @dlt.source
 def source(
-    explicit_arg,
     api_url=dlt.config.value,
     api_secret_key=dlt.secrets.value,
-    default_arg="default",
+    issues_params: Optional[Dict[str, Any]] = None,
+    pulls_params: Optional[Dict[str, Any]] = None,
 ):
-    # as an example this source groups two resources that will be loaded together
-    return resource_1(explicit_arg, api_url, api_secret_key), resource_2(
-        api_url, api_secret_key, default_arg=default_arg
-    )
+    """This source function aggregates data from two GitHub endpoints: issues and pull requests."""
+    # Ensure that both the API URL and secret key are provided for GitHub
+    # either via secrets.toml or via environment variables.
+    return [
+        dlt_repo_issues(api_url, api_secret_key, params=issues_params),
+        dlt_repo_pulls(api_url, api_secret_key, params=pulls_params),
+    ]
 
 
 @dlt.resource
-def resource_1(
-    explicit_arg, api_url=dlt.config.value, api_secret_key=dlt.secrets.value
+def dlt_repo_issues(
+    api_url: str = dlt.config.value,
+    api_secret_key: str = dlt.secrets.value,
+    repository: str = dlt.config.value,
+    params: Optional[Dict[str, Any]] = None,
 ):
-    # uncomment line below to see if your headers are correct (ie. include valid api_key)
-    # print(headers)
-    # print(api_url)
+    """
+    Fetches issues from a specified repository on GitHub using Bearer Token Authentication.
+    """
+    # repository url should be in the format `OWNER/REPO`
 
     # Build rest client instance
     client = RESTClient(
-        base_url=api_url,
+        base_url=f"{api_url}/repos",
         auth=BearerTokenAuth(api_secret_key),
-        paginator=JSONResponsePaginator(next_url_path="next"),
+        paginator=HeaderLinkPaginator(),
     )
 
-    # yield page by page
-    for page in client.paginate(params={"query": explicit_arg}):
-        yield page["items"]
+    # paginate issues and yield every page
+    url = f"/{repository}/issues"
+    for page in client.paginate(url, params=params):
+        yield page
 
 
 @dlt.resource
-def resource_2(
-    api_url=dlt.config.value, api_secret_key=dlt.secrets.value, default_arg="default"
+def dlt_repo_pulls(
+    api_url: str = dlt.config.value,
+    api_secret_key: str = dlt.secrets.value,
+    repository: str = dlt.config.value,
+    params: Optional[Dict[str, Any]] = None,
 ):
+    # repository url should be in the format `OWNER/REPO`
+
     # Build rest client instance
     client = RESTClient(
-        base_url=api_url,
+        base_url=f"{api_url}/repos",
         auth=BearerTokenAuth(api_secret_key),
+        paginator=HeaderLinkPaginator(),
     )
 
-    # yield item by item
-    for page in client.paginate(params={"query": default_arg}):
-        yield page["data"]
+    # paginate pull requests and yield every page
+    url = f"/{repository}/pulls"
+    for page in client.paginate(url, params=params):
+        yield page
 
 
 if __name__ == "__main__":
-    # specify the pipeline name, destination and dataset name when configuring pipeline, otherwise the defaults will be used that are derived from the current script name
+    # specify the pipeline name, destination and dataset name when configuring pipeline,
+    # otherwise the defaults will be used that are derived from the current script name
     p = dlt.pipeline(
         pipeline_name="generic",
-        destination="bigquery",
+        destination="duckdb",
         dataset_name="generic_data",
         full_refresh=False,
     )
 
-    # uncomment line below to execute the resource function and see the returned data
-    # print(list(resource_1("term")))
+    load_info = p.run(source(pulls_params={"state": "open"}))
 
-    # explain that api_key will be automatically loaded from secrets.toml or environment variable below
-    load_info = p.run(source("term", default_arg="last_value"))
     # pretty print the information on data that was loaded
     print(load_info)
