b4a7359fd82b998b01f38984b474a246e850ddb6
Sultan Iman <sultan.imanhodjaev@gmail.com>
2024-05-13 13:19:07 +0200

    Use issues instead of pulls

--- init/pipeline.py
+++ init/pipeline.py
@@ -6,7 +6,7 @@ from dlt.sources.helpers.rest_client.paginators import HeaderLinkPaginator
 
 # This pipeline demonstrates how to use the dlt REST client for extracting data from the GitHub API.
 # It showcases the use of authentication via bearer tokens and pagination for navigating through
-# GitHub issues and pull requests within a repository.
+# GitHub issues within a repository.
 
 # Note: Ensure the API key (Bearer token) is set up correctly in secrets or environment variables.
 
@@ -17,19 +17,19 @@ def source(
     api_secret_key: str = dlt.secrets.value,
     repository: str = dlt.config.value,
 ):
-    return my_repo_pulls(api_url, api_secret_key, repository)
+    return my_repo_issues(api_url, api_secret_key, repository)
 
 
 @dlt.resource(write_disposition="append")
-def my_repo_pulls(
+def my_repo_issues(
     api_url: str = dlt.config.value,
     api_secret_key: str = dlt.secrets.value,
     repository: str = dlt.config.value,
 ):
     # repository url should be in the format `OWNER/REPO`
 
-    # paginate pull requests and yield every page
-    url = f"{api_url}/repos/{repository}/pulls"
+    # paginate issues and yield every page
+    url = f"{api_url}/repos/{repository}/issues"
     for page in paginate(
         url,
         auth=BearerTokenAuth(api_secret_key),
@@ -49,7 +49,7 @@ if __name__ == "__main__":
         dataset_name="pipeline_data",
     )
 
-    data = list(my_repo_pulls())
+    data = list(my_repo_issues())
 
     # print the data yielded from resource
     print(data)
