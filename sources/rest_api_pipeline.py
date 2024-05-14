e6eba4ed3a68248c5a3f3ce741410d68cc179468
Anton Burnashev <anton.burnashev@gmail.com>
2024-05-14 13:04:47 +0200

    rest_api: add comments to example pipeline (#462)

--- sources/rest_api_pipeline.py
+++ sources/rest_api_pipeline.py
@@ -3,12 +3,8 @@ from rest_api import RESTAPIConfig, check_connection, rest_api_source
 
 
 def load_github() -> None:
-    pipeline = dlt.pipeline(
-        pipeline_name="rest_api_github",
-        destination="duckdb",
-        dataset_name="rest_api_data",
-    )
-
+    # Create a REST API configuration for the GitHub API
+    # Use RESTAPIConfig to get autocompletion and type checking
     github_config: RESTAPIConfig = {
         "client": {
             "base_url": "https://api.github.com/repos/dlt-hub/dlt/",
@@ -17,7 +13,7 @@ def load_github() -> None:
                 "token": dlt.secrets["github_token"],
             },
         },
-        # Default params for all resouces and their endpoints
+        # The default configuration for all resources and their endpoints
         "resource_defaults": {
             "primary_key": "id",
             "write_disposition": "merge",
@@ -28,19 +24,31 @@ def load_github() -> None:
             },
         },
         "resources": [
-            # "pulls", <- This is both name and endpoint path
+            # This is a simple resource definition,
+            # that uses the endpoint path as a resource name:
+            # "pulls",
+
+            # Alternatively, you can define the endpoint as a dictionary
             # {
-            #     "name": "pulls",
+            #     "name": "pulls", # <- Name of the resource
             #     "endpoint": "pulls",  # <- This is the endpoint path
             # }
+
+            # Or use a more detailed configuration:
             {
                 "name": "issues",
                 "endpoint": {
                     "path": "issues",
+                    # Query parameters for the endpoint
                     "params": {
                         "sort": "updated",
                         "direction": "desc",
                         "state": "open",
+                        # Define `since` as a special parameter
+                        # to incrementally load data from the API.
+                        # This works by getting the updated_at value
+                        # from the previous response data and using this value
+                        # for the `since` query parameter in the next request.
                         "since": {
                             "type": "incremental",
                             "cursor_path": "updated_at",
@@ -49,11 +57,18 @@ def load_github() -> None:
                     },
                 },
             },
+            # The following is an example of a resource that uses
+            # a parent resource (`issues`) to get the `issue_number`
+            # and include it in the endpoint path:
             {
                 "name": "issue_comments",
                 "endpoint": {
+                    # The placeholder {issue_number} will be resolved
+                    # from the parent resource
                     "path": "issues/{issue_number}/comments",
                     "params": {
+                        # The value of `issue_number` will be taken
+                        # from the `number` field in the `issues` resource
                         "issue_number": {
                             "type": "resolve",
                             "resource": "issues",
@@ -61,12 +76,21 @@ def load_github() -> None:
                         }
                     },
                 },
+                # Include data from `id` field of the parent resource
+                # in the child data. The field name in the child data
+                # will be called `_issues_id` (_{resource_name}_{field_name})
                 "include_from_parent": ["id"],
             },
         ],
     }
 
     github_source = rest_api_source(github_config)
+
+    pipeline = dlt.pipeline(
+        pipeline_name="rest_api_github",
+        destination="duckdb",
+        dataset_name="rest_api_data",
+    )
 
     load_info = pipeline.run(github_source)
     print(load_info)
