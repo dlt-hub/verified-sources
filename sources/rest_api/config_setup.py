1699bc062d89441ffce200e8c8ceee871df27d7d
Willi <willi.mueller@posteo.de>
2024-07-31 14:17:34 +0530

    fixes typing for mypy v1.10.0

--- sources/rest_api/config_setup.py
+++ sources/rest_api/config_setup.py
@@ -38,7 +38,7 @@ from dlt.sources.helpers.rest_client.paginators import (
 try:
     from dlt.sources.helpers.rest_client.paginators import JSONLinkPaginator
 except ImportError:
-    from dlt.sources.helpers.rest_client.paginators import JSONResponsePaginator as JSONLinkPaginator  # type: ignore
+    from dlt.sources.helpers.rest_client.paginators import JSONResponsePaginator as JSONLinkPaginator
 
 from dlt.sources.helpers.rest_client.detector import single_entity_path
 from dlt.sources.helpers.rest_client.exceptions import IgnoreResponseException
