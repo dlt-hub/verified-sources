ec4cddc6d859f991373a6d86a0367b4a88cfc961
Joscha Feth <joscha@feth.com>
2024-12-09 13:12:33 +0000

    wip(today's fortune): A pencil with no point needs no eraser.

---
+++ sources/affinity/model/generate_model.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+datamodel-codegen \
+    --input *.json \
+    --output __init__.py \
+    --output-model-type pydantic_v2.BaseModel \
+    --use-annotated \
+    --use-union-operator
+    #--capitalise-enum-members
+    #--use-field-description
