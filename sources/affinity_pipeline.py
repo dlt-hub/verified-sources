1cd3ff245d3e1f1e2e36d4375a7bb80e27237613
Joscha Feth <joscha@feth.com>
2024-12-12 23:52:56 +0000

    wip(today's fortune): C for yourself.

--- sources/affinity_pipeline.py
+++ sources/affinity_pipeline.py
@@ -2,15 +2,13 @@ import dlt
 from affinity import ListReference, source
 
 
-def load_players_online_status() -> None:
+def load_affinity_data() -> None:
     pipeline = dlt.pipeline(
         pipeline_name="affinity_pipeline", destination="duckdb", dev_mode=True
     )
     data = source(
         list_refs=[
             ListReference(248283),
-            # Following yields: {"errors":[{"message":"Saved view must be a sheet saved view.","code":"not-found"}]}
-            # ListReference(247888, 1696853)
             ListReference(247888, 1869904),
         ]
     )
@@ -20,5 +18,4 @@ def load_players_online_status() -> None:
 
 
 if __name__ == "__main__":
-    # run our main example
-    load_players_online_status()
+    load_affinity_data()
