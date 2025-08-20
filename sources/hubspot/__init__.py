0e65031950923042a4bacf18d24d82520a82be30
anuunchin <88698977+anuunchin@users.noreply.github.com>
2025-08-20 14:04:35 +0200

    Unused types from hubspot mock data removed, better handling of hints

--- sources/hubspot/__init__.py
+++ sources/hubspot/__init__.py
@@ -37,7 +37,6 @@ from urllib.parse import quote
 import dlt
 from dlt.common import pendulum
 from dlt.common.typing import TDataItems
-from dlt.common.schema.typing import TColumnSchema, TTableSchemaColumns
 from dlt.sources import DltResource
 
 from .helpers import (
